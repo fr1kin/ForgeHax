1f964bfee27e5a8853b5371813b85c0a04b2c3e5
fr1kin <matt.fr1kin@gmail.com>
2017-06-27 00:12:01 -0500

    rewrote angle class (not completely implemented yet)

---
+++ src/main/java/com/matt/forgehax/util/math/AngleHelper.java
@@ -0,0 +1,58 @@
+package com.matt.forgehax.util.math;
+
+import net.minecraft.util.math.Vec3d;
+
+/**
+ * Created on 6/21/2017 by fr1kin
+ */
+public class AngleHelper {
+    public static final long DEFAULT_N = 1000000000; // 9 decimal places
+    public static final double DEFAULT_EPSILON = (1.D / (double)DEFAULT_N);
+
+    public static final double TWO_PI = Math.PI * 2.D; // 180 degrees
+    public static final double HALF_PI = Math.PI / 2.D; // 90 degrees
+    public static final double QUARTER_PI = Math.PI / 4.D; // 45 degrees
+
+    public static double roundAngle(double a, long n) {
+        return Math.round(a * n) / n;
+    }
+    public static double roundAngle(double a) {
+        return roundAngle(a, DEFAULT_N);
+    }
+
+    public static boolean isAngleEqual(double a1, double a2, double epsilon) {
+        return Double.compare(a1, a2) == 0 || Math.abs(a1 - a2) < epsilon;
+    }
+    public static boolean isAngleEqual(double a1, double a2) {
+        return isAngleEqual(a1, a2, DEFAULT_EPSILON);
+    }
+
+    public static double normalizeInRadians(double ang) {
+        while(ang > Math.PI) ang -= 2 * Math.PI;
+        while(ang < -Math.PI) ang += 2 * Math.PI;
+        return ang;
+    }
+
+    public static double normalizeInDegrees(double ang) {
+        while (ang <= -180) ang += 360;
+        while (ang > 180) ang -= 360;
+        return ang;
+    }
+
+    public static AngleN getAngleFacingInRadians(Vec3d vector) {
+        double pitch, yaw;
+        if(vector.xCoord == 0 && vector.zCoord == 0) {
+            yaw = 0.D;
+            pitch = HALF_PI;
+        } else {
+            yaw = Math.atan2(vector.zCoord, vector.xCoord) - HALF_PI;
+            double mag = Math.sqrt(vector.xCoord * vector.xCoord + vector.zCoord * vector.zCoord);
+            pitch = -Math.atan2(vector.yCoord, mag);
+        }
+        return AngleN.radians(pitch, yaw);
+    }
+
+    public static AngleN getAngleFacingInDegrees(Vec3d vector) {
+        return getAngleFacingInRadians(vector).toDegrees();
+    }
+}
