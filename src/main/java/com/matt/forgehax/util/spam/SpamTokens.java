28a1256b617393782b8bb3e54fc7706a626543c0
fr1kin <matt.fr1kin@gmail.com>
2017-07-20 17:09:38 -0500

    added way to prevent bad goyim character escaping (not that it matters)

--- src/main/java/com/matt/forgehax/util/spam/SpamTokens.java
+++ src/main/java/com/matt/forgehax/util/spam/SpamTokens.java
@@ -1,5 +1,8 @@
 package com.matt.forgehax.util.spam;
 
+import com.google.common.escape.CharEscaperBuilder;
+import com.google.common.escape.Escaper;
+
 /**
  * Created on 7/18/2017 by fr1kin
  */
@@ -7,6 +10,10 @@ public enum SpamTokens {
     PLAYER_NAME("PLAYER_NAME")
 
     ;
+    public static Escaper BAD_GOY_PREVENTER = new CharEscaperBuilder()
+            .addEscape('\\', "\\\\") //TODO: find out more bad goy methods for attempting escaping
+            .toEscaper();
+
     final String token;
 
     SpamTokens(String token) {
@@ -18,6 +25,6 @@ public enum SpamTokens {
     }
 
     public String fill(String str, String with) {
-        return str.replaceAll(token, with);
+        return str.replaceAll(token, BAD_GOY_PREVENTER.escape(with));
     }
 }
