45bf8d1aa8385eba16522b7a9c3ae903d09d5667
fr1kin <matt.fr1kin@gmail.com>
2017-07-22 23:22:35 -0500

    added wait

--- src/main/java/com/matt/forgehax/mods/services/ScoreboardListenerService.java
+++ src/main/java/com/matt/forgehax/mods/services/ScoreboardListenerService.java
@@ -2,6 +2,7 @@ package com.matt.forgehax.mods.services;
 
 import com.matt.forgehax.asm.events.PacketEvent;
 import com.matt.forgehax.events.PlayerConnectEvent;
+import com.matt.forgehax.util.command.Setting;
 import com.matt.forgehax.util.entity.PlayerInfo;
 import com.matt.forgehax.util.entity.PlayerInfoHelper;
 import com.matt.forgehax.util.mod.ServiceMod;
@@ -10,15 +11,24 @@ import com.mojang.authlib.GameProfile;
 import joptsimple.internal.Strings;
 import net.minecraft.network.play.server.SPacketPlayerListItem;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.WorldEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.Objects;
+
+import static com.matt.forgehax.Helper.getWorld;
 
 /**
  * Created on 7/18/2017 by fr1kin
  */
 @RegisterMod
 public class ScoreboardListenerService extends ServiceMod {
+    public final Setting<Integer> wait = getCommandStub().builders().<Integer>newSettingBuilder()
+            .name("wait")
+            .description("Time to wait after joining world")
+            .defaultTo(5000)
+            .build();
+
     public ScoreboardListenerService() {
         super("ScoreboardListenerService", "Listens for player joining and leaving");
     }
@@ -36,9 +46,18 @@ public class ScoreboardListenerService extends ServiceMod {
         }
     }
 
+    private long waitTime = 0;
+
+    @SubscribeEvent
+    public void onWorldLoad(WorldEvent.Load event) {
+        waitTime = System.currentTimeMillis() + wait.get();
+    }
+
     @SubscribeEvent
     public void onScoreboardEvent(PacketEvent.Incoming.Pre event) {
-        if(event.getPacket() instanceof SPacketPlayerListItem) {
+        if(event.getPacket() instanceof SPacketPlayerListItem
+                && getWorld() != null
+                && System.currentTimeMillis() > waitTime) {
             final SPacketPlayerListItem packet = (SPacketPlayerListItem) event.getPacket();
             packet.getEntries().stream()
                     .filter(Objects::nonNull)
