5ea5b7d4ff467afe7683010cccf0fd9019a8f02a
fr1kin <matt.fr1kin@gmail.com>
2017-08-11 15:52:23 -0500

    WIP tracers, now look better

--- src/main/java/com/matt/forgehax/mods/Tracers.java
+++ src/main/java/com/matt/forgehax/mods/Tracers.java
@@ -1,15 +1,17 @@
 package com.matt.forgehax.mods;
 
+import com.matt.forgehax.util.Utils;
 import com.matt.forgehax.util.draw.SurfaceUtils;
 import com.matt.forgehax.util.entity.EntityUtils;
+import com.matt.forgehax.util.math.AngleHelper;
 import com.matt.forgehax.util.math.VectorUtils;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
-import net.minecraft.client.entity.AbstractClientPlayer;
-import net.minecraft.util.ResourceLocation;
+import net.minecraft.entity.EntityLiving;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.lwjgl.util.vector.Vector2f;
 
 import java.util.Objects;
 
@@ -27,31 +29,49 @@ public class Tracers extends ToggleMod {
 
     @SubscribeEvent
     public void onDrawScreen(RenderGameOverlayEvent.Text event) {
-        final double dX = (double) MC.displayWidth / 4.D;
-        final double dY = (double) MC.displayHeight / 4.D;
+        final Vector2f origin = new Vector2f(0.f, 0.f);
+        final Vector2f center = new Vector2f(MC.displayWidth / 4.f, MC.displayHeight / 4.f);
         getWorld().loadedEntityList.stream()
                 .filter(entity -> !Objects.equals(entity, getLocalPlayer()))
-                .filter(entity -> entity instanceof AbstractClientPlayer)
+                .filter(entity -> entity instanceof EntityLiving)
                 .forEach(entity -> {
                     Vec3d pos3d = EntityUtils.getInterpolatedEyePos(entity, MC.getRenderPartialTicks());
                     VectorUtils.ScreenPos pos = VectorUtils.toScreen(pos3d);
                     if(!pos.isVisible) {
-                        double cx = pos.x, cy = pos.y;
+                        double ex = (pos.x - center.x) / center.x;
+                        double ey = (pos.y - center.y) / center.y;
 
-                        double x = (cx - dX) / dX;
-                        double y = (cy - dY) / dY;
+                        double m = Math.abs(Math.sqrt(ex*ex + ey*ey));
+                        double nx = ex / m;
+                        double ny = ey / m;
 
-                        double mag = Math.abs(Math.sqrt(x*x + y*y));
-                        double normX = x / mag;
-                        double normY = y / mag;
+                        double x = center.x + nx * center.x;
+                        double y = center.y + ny * center.y;
 
-                        double prodX = dX + normX * dX;
-                        double prodY = dY + normY * dY;
+                        // point - center
+                        // w = <px - cx, py - cy>
+                        Vector2f w = new Vector2f((float) x, (float) y);
+                        w.x -= center.x;
+                        w.y -= center.y;
 
-                        ResourceLocation resourceLocation = AbstractClientPlayer.getLocationSkin(entity.getName());
-                        AbstractClientPlayer.getDownloadImageSkin(resourceLocation, entity.getName());
+                        // u = <w, h/2>
+                        Vector2f u = new Vector2f(MC.displayWidth / 2.f, origin.y);
 
-                        SurfaceUtils.drawHead(resourceLocation, (int)prodX, (int)prodY, 1);
+                        // |u|
+                        float mu = u.length();
+                        // |w|
+                        float mw = w.length();
+
+                        // theta = dot(u,w)/(|u|*|w|)
+                        float ang = (float) Math.toDegrees(Math.acos((u.x*w.x + u.y*w.y)/(mu*mw)));
+
+                        // invert if above
+                        if(y < center.y) ang *= -1;
+
+                        // normalize
+                        ang = (float) AngleHelper.normalizeInDegrees(ang);
+
+                        SurfaceUtils.drawTriangle((int) x, (int) y, 5, ang, Utils.Colors.GREEN);
                     }
                 });
     }
