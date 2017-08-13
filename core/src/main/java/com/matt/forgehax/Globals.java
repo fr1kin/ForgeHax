ec5b5d7444cb9d867982cbdceab5f73b83f228d0
fr1kin <matt.fr1kin@gmail.com>
2017-08-13 14:58:28 -0500

    moved forgehax core to separate subproject

---
+++ core/src/main/java/com/matt/forgehax/Globals.java
@@ -0,0 +1,17 @@
+package com.matt.forgehax;
+
+import com.matt.forgehax.util.command.Command;
+import com.matt.forgehax.util.command.CommandGlobal;
+import net.minecraft.client.Minecraft;
+import net.minecraftforge.fml.client.FMLClientHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 2 lazy to import static
+ */
+public interface Globals {
+    Logger      LOGGER              = LogManager.getLogger("ForgeHax");
+    Minecraft   MC                  = FMLClientHandler.instance().getClient();
+    Command     GLOBAL_COMMAND      = CommandGlobal.getInstance();
+}
---
+++ core/src/main/java/com/matt/forgehax/mods/AntiOverlayMod.java
@@ -0,0 +1,41 @@
+package com.matt.forgehax.mods;
+
+import com.matt.forgehax.util.mod.ToggleMod;
+import com.matt.forgehax.util.mod.loader.RegisterMod;
+import net.minecraft.block.material.Material;
+import net.minecraftforge.client.event.*;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+@RegisterMod
+public class AntiOverlayMod extends ToggleMod {
+    public AntiOverlayMod() {
+        super("AntiOverlay", false, "Removes screen overlays");
+    }
+
+    /**
+     * Disables water/lava fog
+     */
+    @SubscribeEvent
+    public void onFogRender(EntityViewRenderEvent.FogDensity event) {
+        if(event.getState().getMaterial().equals(Material.WATER) ||
+                event.getState().getMaterial().equals(Material.LAVA)) {
+            event.setDensity(0);
+            event.setCanceled(true);
+        }
+    }
+
+    /**
+     * Disables screen overlays
+     */
+    @SubscribeEvent
+    public void onRenderBlockOverlay(RenderBlockOverlayEvent event) { event.setCanceled(true); }
+
+    @SubscribeEvent
+    public void onRenderGameOverlay(RenderGameOverlayEvent event) {
+        if (event.getType().equals(RenderGameOverlayEvent.ElementType.HELMET) ||
+                event.getType().equals(RenderGameOverlayEvent.ElementType.PORTAL))
+            event.setCanceled(true);
+    }
+
+
+}
