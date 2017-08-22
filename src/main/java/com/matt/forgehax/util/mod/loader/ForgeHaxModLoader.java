869fd6cb2bb3ae5e0b5dd2906a1271c655d8c477
fr1kin <matt.fr1kin@gmail.com>
2017-08-22 16:32:47 -0500

    removed unused method

--- src/main/java/com/matt/forgehax/util/mod/loader/ForgeHaxModLoader.java
+++ src/main/java/com/matt/forgehax/util/mod/loader/ForgeHaxModLoader.java
@@ -1,7 +1,6 @@
 package com.matt.forgehax.util.mod.loader;
 
 import com.google.common.collect.Lists;
-import com.google.common.reflect.ClassPath;
 import com.matt.forgehax.Globals;
 import com.matt.forgehax.Helper;
 import com.matt.forgehax.util.mod.BaseMod;
@@ -30,20 +29,6 @@ public class ForgeHaxModLoader implements Globals {
             Helper.handleThrowable(t);
         }
         return Collections.emptyList();
-    }
-
-    @SuppressWarnings("unchecked")
-    protected static Collection<Class<? extends BaseMod>> filterClassInfo(Collection<ClassPath.ClassInfo> input) {
-        final List<Class<? extends BaseMod>> classes = Lists.newArrayList();
-        input.forEach(info -> {
-            try {
-                Class<?> clazz = info.load();
-                if(isClassValid(clazz)) classes.add((Class<? extends BaseMod>)clazz);
-            } catch (Exception e) {
-                getLog().warn(String.format("[%s] '%s' is not a valid mod class: %s", e.getClass().getSimpleName(), info.getSimpleName(), e.getMessage()));
-            }
-        });
-        return Collections.unmodifiableCollection(classes);
     }
 
     protected static boolean isClassValid(Class<?> clazz) {
