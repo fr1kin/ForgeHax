6d12078fae32362f13d40e235c7e855fff0dd8ec
fr1kin <matt.fr1kin@gmail.com>
2017-08-30 18:45:28 -0500

    made all methods @Deprecated

--- src/main/java/com/matt/forgehax/util/entity/PlayerUtils.java
+++ src/main/java/com/matt/forgehax/util/entity/PlayerUtils.java
@@ -1,16 +1,22 @@
 package com.matt.forgehax.util.entity;
 
 import com.matt.forgehax.Globals;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.player.EntityPlayer;
 
 import static com.matt.forgehax.Helper.getLocalPlayer;
 
 public class PlayerUtils implements Globals {
-    public static boolean isLocalPlayer(EntityPlayer player) {
+    /**
+     * Use EntityUtils::isLocalPlayer
+     */
+    @Deprecated
+    public static boolean isLocalPlayer(Entity player) {
         EntityPlayer localPlayer = getLocalPlayer();
         return localPlayer != null && localPlayer.equals(player);
     }
 
+    @Deprecated
     public static boolean isFriend(EntityPlayer player) {
         return false;
     }
