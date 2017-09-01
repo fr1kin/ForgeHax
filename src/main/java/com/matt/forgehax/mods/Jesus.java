23bdbc7a78d55c65876eb219051a20d4d3ed8e42
babbaj <babbaj1@yahoo.com>
2017-08-31 19:13:38 -0500

    Jesus no longer works for blocks not below the player
    allows the player to swim up waterfalls
    
    also forgot to add param to isAboveWater

--- src/main/java/com/matt/forgehax/mods/Jesus.java
+++ src/main/java/com/matt/forgehax/mods/Jesus.java
@@ -48,7 +48,8 @@ public class Jesus extends ToggleMod {
                 && !getLocalPlayer().isSneaking()
                 && getLocalPlayer().fallDistance < 3
                 && !isInWater(getLocalPlayer())
-                && isAboveWater(getLocalPlayer(), false)) {
+                && isAboveWater(getLocalPlayer(), false)
+                && isAboveBlock(getLocalPlayer(), event.getPos())) {
             AxisAlignedBB axisalignedbb = WATER_WALK_AA.offset(event.getPos());
             if (event.getEntityBox().intersects(axisalignedbb)) event.getCollidingBoxes().add(axisalignedbb);
             // cancel event, which will stop it from calling the original code
@@ -84,7 +85,7 @@ public class Jesus extends ToggleMod {
         return false;
     }
 
-    private static boolean isAboveWater(Entity entity){
+    private static boolean isAboveWater(Entity entity, boolean packet){
         double y = entity.posY - (packet ? 0.03 : 0.2); // increasing this seems to flag more in NCP but needs to be increased so the player lands on solid water
 
         for(int x = MathHelper.floor(entity.posX); x < MathHelper.ceil(entity.posX); x++)
@@ -110,6 +111,10 @@ public class Jesus extends ToggleMod {
             }
 
         return false;
+    }
+    
+    private static boolean isAboveBlock(Entity entity, BlockPos pos) {
+        return entity.posY  >= pos.getY();
     }
 
 }
