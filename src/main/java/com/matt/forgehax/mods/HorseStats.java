3f8ef01cabb9489c4d18d923a59bcdd01af15b4f
babbaj <babbaj1@yahoo.com>
2017-09-02 01:56:32 -0500

    reee didnt import

--- src/main/java/com/matt/forgehax/mods/HorseStats.java
+++ src/main/java/com/matt/forgehax/mods/HorseStats.java
@@ -2,6 +2,7 @@ package com.matt.forgehax.mods;
 
 import com.matt.forgehax.asm.reflection.FastReflection;
 import com.matt.forgehax.util.command.Setting;
+import com.matt.forgehax.util.entity.EntityUtils;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
 import net.minecraft.entity.EntityLivingBase;
