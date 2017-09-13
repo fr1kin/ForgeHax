d43c531be7244e8ec3407a81c849bbba037c669c
fr1kin <matt.fr1kin@gmail.com>
2017-09-12 22:12:08 -0500

    Commands are now scanned from the global command

--- src/main/java/com/matt/forgehax/mods/services/BindEventService.java
+++ src/main/java/com/matt/forgehax/mods/services/BindEventService.java
@@ -1,15 +1,12 @@
 package com.matt.forgehax.mods.services;
 
-import com.matt.forgehax.util.command.Command;
 import com.matt.forgehax.util.command.CommandStub;
 import com.matt.forgehax.util.mod.ServiceMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.InputEvent;
 
-import java.util.Collection;
-
-import static com.matt.forgehax.Helper.getModManager;
+import static com.matt.forgehax.Helper.getGlobalCommand;
 
 /**
  * Created on 6/14/2017 by fr1kin
@@ -22,18 +19,13 @@ public class BindEventService extends ServiceMod {
 
     @SubscribeEvent
     public void onKeyboardEvent(InputEvent.KeyInputEvent event) {
-        getModManager().getMods().forEach(mod -> {
-            Collection<Command> cmds = mod.getCommandStub().getChildrenDeep();
-            cmds.add(mod.getCommandStub());
-            cmds.forEach(c -> {
-                if(c instanceof CommandStub) {
-                    CommandStub stub = (CommandStub) c;
-                    if (stub.getBind() != null) {
-                        if (stub.getBind().isPressed()) stub.onKeyPressed();
-                        if (stub.getBind().isKeyDown()) stub.onKeyDown();
-                    }
-                }
-            });
-        });
+        getGlobalCommand().getChildrenDeep().stream()
+                .filter(command -> command instanceof CommandStub)
+                .map(command -> (CommandStub)command)
+                .filter(stub -> stub.getBind() != null)
+                .forEach(stub -> {
+                    if (stub.getBind().isPressed()) stub.onKeyPressed();
+                    if (stub.getBind().isKeyDown()) stub.onKeyDown();
+                });
     }
 }
