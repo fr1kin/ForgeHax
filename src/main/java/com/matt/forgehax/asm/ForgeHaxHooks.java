70ef7d9b9c4e9adb8e0a2a004afa9841b446f150
fr1kin <matt.fr1kin@gmail.com>
2018-02-04 22:26:54 -0600

    Removed old hook debugging

--- src/main/java/com/matt/forgehax/asm/ForgeHaxHooks.java
+++ src/main/java/com/matt/forgehax/asm/ForgeHaxHooks.java
@@ -1,7 +1,5 @@
 package com.matt.forgehax.asm;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.matt.forgehax.asm.events.*;
 import com.matt.forgehax.asm.events.listeners.BlockModelRenderListener;
@@ -27,56 +25,10 @@ import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
 
 import java.nio.ByteOrder;
-import java.util.Collections;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 public class ForgeHaxHooks implements ASMCommon {
-    public static boolean isInDebugMode = true;
-
-    public final static Map<String, DebugData> responding = Maps.newLinkedHashMap();
-
-    static {
-        responding.put("onHurtcamEffect",                   new DebugData("net.minecraft.client.renderer.EntityRenderer"));
-        responding.put("onSendingPacket",                   new DebugData("net.minecraft.network.NetworkManager", "net.minecraft.network.NetworkManager$4"));
-        responding.put("onSentPacket",                      new DebugData("net.minecraft.network.NetworkManager", "net.minecraft.network.NetworkManager$4"));
-        responding.put("onPreReceived",                     new DebugData("net.minecraft.network.NetworkManager"));
-        responding.put("onPostReceived",                    new DebugData("net.minecraft.network.NetworkManager"));
-        responding.put("onWaterMovement",                   new DebugData("net.minecraft.world.World"));
-        responding.put("onApplyCollisionMotion",            new DebugData("net.minecraft.entity.Entity"));
-        responding.put("onWebMotion",                       new DebugData("net.minecraft.entity.Entity"));
-        responding.put("onDoBlockCollisions",               new DebugData("net.minecraft.entity.Entity"));
-        responding.put("onPutColorMultiplier",              new DebugData("net.minecraft.client.renderer.BufferBuilder"));
-        responding.put("onPreRenderBlockLayer",             new DebugData("net.minecraft.client.renderer.RenderGlobal"));
-        responding.put("onPostRenderBlockLayer",            new DebugData("net.minecraft.client.renderer.RenderGlobal"));
-        responding.put("onRenderBlockInLayer",              new DebugData("net.minecraft.block.Block"));
-        responding.put("onSetupTerrain",                    new DebugData("net.minecraft.client.renderer.RenderGlobal"));
-        responding.put("onComputeVisibility",               new DebugData("net.minecraft.client.renderer.chunk.VisGraph"));
-        responding.put("onPushOutOfBlocks",                 new DebugData("net.minecraft.client.entity.EntityPlayerSP"));
-        responding.put("onRenderBoat",                      new DebugData("net.minecraft.client.renderer.entity.RenderBoat"));
-        responding.put("onAddCollisionBoxToList",           new DebugData("net.minecraft.block.Block"));
-    }
-
-    private static void reportHook(String name) {
-        if(isInDebugMode) {
-            DebugData debug = responding.get(name);
-            if (debug != null) {
-                debug.hasResponded = true;
-                debug.isResponding = true;
-            }
-        }
-    }
-
-    public static void setHooksLog(String className, final List<String> log, int methodCount) {
-        for(DebugData data : responding.values()) {
-            if(data.parentClassNames.contains(className)) {
-                data.log = log;
-                data.targetCount = methodCount;
-            }
-        }
-    }
-
     /**
      * static fields
      */
@@ -98,54 +50,44 @@ public class ForgeHaxHooks implements ASMCommon {
      */
 
     public static boolean onPushOutOfBlocks() {
-        reportHook("onPushOutOfBlocks");
         return MinecraftForge.EVENT_BUS.post(new PushOutOfBlocksEvent());
     }
 
     public static float onRenderBoat(EntityBoat boat, float entityYaw) {
-        reportHook("onRenderBoat");
         RenderBoatEvent event = new RenderBoatEvent(boat, entityYaw);
         MinecraftForge.EVENT_BUS.post(event);
         return event.getYaw();
     }
 
     public static void onSchematicaPlaceBlock(ItemStack itemIn, BlockPos posIn, Vec3d vecIn) {
-        reportHook("onSchematicaPlaceBlock");
         MinecraftForge.EVENT_BUS.post(new SchematicaPlaceBlockEvent(itemIn, posIn, vecIn));
     }
 
     public static boolean onHurtcamEffect(float partialTicks) {
-        reportHook("onHurtcamEffect");
         return MinecraftForge.EVENT_BUS.post(new HurtCamEffectEvent(partialTicks));
     }
 
     public static boolean onSendingPacket(Packet<?> packet) {
-        reportHook("onSendingPacket");
         return MinecraftForge.EVENT_BUS.post(new PacketEvent.Outgoing.Pre(packet));
     }
 
     public static void onSentPacket(Packet<?> packet) {
-        reportHook("onSentPacket");
         MinecraftForge.EVENT_BUS.post(new PacketEvent.Outgoing.Post(packet));
     }
 
     public static boolean onPreReceived(Packet<?> packet) {
-        reportHook("onPreReceived");
         return MinecraftForge.EVENT_BUS.post(new PacketEvent.Incoming.Pre(packet));
     }
 
     public static void onPostReceived(Packet<?> packet) {
-        reportHook("onPostReceived");
         MinecraftForge.EVENT_BUS.post(new PacketEvent.Incoming.Post(packet));
     }
 
     public static boolean onWaterMovement(Entity entity, Vec3d moveDir) {
-        reportHook("onWaterMovement");
         return MinecraftForge.EVENT_BUS.post(new WaterMovementEvent(entity, moveDir));
     }
 
     public static boolean onApplyCollisionMotion(Entity entity, Entity collidedWithEntity, double x, double z) {
-        reportHook("onApplyCollisionMotion");
         return MinecraftForge.EVENT_BUS.post(new ApplyCollisionMotionEvent(entity, collidedWithEntity, x, 0.D, z));
     }
 
@@ -153,7 +95,6 @@ public class ForgeHaxHooks implements ASMCommon {
     public static float COLOR_MULTIPLIER_ALPHA = 150.f / 255.f;
 
     public static int onPutColorMultiplier(float r, float g, float b, int buffer, boolean[] flag) {
-        reportHook("onPutColorMultiplier");
         flag[0] = SHOULD_UPDATE_ALPHA;
         if(SHOULD_UPDATE_ALPHA) {
             if (ByteOrder.nativeOrder() == ByteOrder.LITTLE_ENDIAN) {
@@ -174,29 +115,24 @@ public class ForgeHaxHooks implements ASMCommon {
     }
 
     public static boolean onPreRenderBlockLayer(BlockRenderLayer layer, double partialTicks) {
-        reportHook("onPreRenderBlockLayer");
         return MinecraftForge.EVENT_BUS.post(new RenderBlockLayerEvent.Pre(layer, partialTicks));
     }
 
     public static void onPostRenderBlockLayer(BlockRenderLayer layer, double partialTicks) {
-        reportHook("onPostRenderBlockLayer");
         MinecraftForge.EVENT_BUS.post(new RenderBlockLayerEvent.Post(layer, partialTicks));
     }
 
     public static boolean onSetupTerrain(Entity renderEntity, boolean playerSpectator) {
-        reportHook("onSetupTerrain");
         SetupTerrainEvent event = new SetupTerrainEvent(renderEntity, playerSpectator);
         MinecraftForge.EVENT_BUS.post(event);
         return event.isCulling();
     }
 
     public static void onComputeVisibility(VisGraph visGraph, SetVisibility setVisibility) {
-        reportHook("onComputeVisibility");
         MinecraftForge.EVENT_BUS.post(new ComputeVisibilityEvent(visGraph, setVisibility));
     }
 
     public static boolean onDoBlockCollisions(Entity entity, BlockPos pos, IBlockState state) {
-        reportHook("onDoBlockCollisions");
         return MinecraftForge.EVENT_BUS.post(new DoBlockCollisionsEvent(entity, pos, state));
     }
 
@@ -209,7 +145,6 @@ public class ForgeHaxHooks implements ASMCommon {
     }
 
     public static BlockRenderLayer onRenderBlockInLayer(Block block, IBlockState state, BlockRenderLayer layer, BlockRenderLayer compareToLayer) {
-        reportHook("onRenderBlockInLayer");
         RenderBlockInLayerEvent event = new RenderBlockInLayerEvent(block, state, layer, compareToLayer);
         MinecraftForge.EVENT_BUS.post(event);
         return event.getLayer();
@@ -270,29 +205,5 @@ public class ForgeHaxHooks implements ASMCommon {
 
     public static void onUpdateWalkingPlayerPost() {
         MinecraftForge.EVENT_BUS.register(new LocalPlayerUpdateMovementEvent.Post());
-    }
-
-    public static class DebugData {
-        public final List<String> parentClassNames = Lists.newArrayList();
-        /**
-         * If the hook has ever responded
-         */
-        public boolean hasResponded = false;
-
-        /**
-         * If the hook has responded since last refresh
-         */
-        public boolean isResponding = false;
-
-        /**
-         * Reference to log from class transformer
-         */
-        public List<String> log;
-
-        public int targetCount = 0;
-
-        public DebugData(String... parentClasses) {
-            Collections.addAll(parentClassNames, parentClasses);
-        }
     }
 }
