0b30241f75967ab82c559d5e2490fea4a90a18c1
fr1kin <matt.fr1kin@gmail.com>
2018-02-11 23:01:08 -0600

    renamed ImmutableCollectors to just Immutables and added some related methods inside

--- src/main/java/com/matt/forgehax/util/Immutables.java
+++ src/main/java/com/matt/forgehax/util/Immutables.java
@@ -1,9 +1,10 @@
 package com.matt.forgehax.util;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableSet;
 
-import java.util.EnumSet;
-import java.util.Set;
+import javax.annotation.Nullable;
+import java.util.*;
 import java.util.function.BiConsumer;
 import java.util.function.BinaryOperator;
 import java.util.function.Function;
@@ -13,7 +14,16 @@ import java.util.stream.Collector;
 /**
  * Created on 2/6/2018 by fr1kin
  */
-public class ImmutableCollectors {
+public class Immutables {
+    public static <T> List<T> copyToList(@Nullable Collection<T> collection) {
+        if(collection == null || collection.isEmpty())
+            return Collections.emptyList();
+        else if(collection.size() == 1)
+            return Collections.singletonList(collection.iterator().next());
+        else
+            return ImmutableList.copyOf(collection);
+    }
+
     // credits to http://www.nurkiewicz.com/2014/07/introduction-to-writing-custom.html
 
     public static <T> Collector<T, ImmutableSet.Builder<T>, ImmutableSet<T>> toImmutableSet() {
