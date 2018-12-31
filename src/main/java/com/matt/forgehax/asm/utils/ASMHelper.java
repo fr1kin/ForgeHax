de30ef06944e26189ce7a5d735d0c167060bf9d5
0x22 <0x22@futureclient.net>
2018-12-31 00:35:20 -0500

    Removed some unused junk in AsmHelper

--- src/main/java/com/matt/forgehax/asm/utils/ASMHelper.java
+++ src/main/java/com/matt/forgehax/asm/utils/ASMHelper.java
@@ -18,44 +18,6 @@ import org.objectweb.asm.tree.*;
 import static org.objectweb.asm.Opcodes.*;
 
 public class ASMHelper {
-  public static final Map<Class<?>, Class<?>> primitiveToWrapper =
-      new HashMap<Class<?>, Class<?>>();
-  public static final Map<Class<?>, Class<?>> wrapperToPrimitive =
-      new HashMap<Class<?>, Class<?>>();
-  public static final Map<Class<?>, Character> primitiveToDescriptor =
-      new HashMap<Class<?>, Character>();
-
-  static {
-    primitiveToWrapper.put(boolean.class, Boolean.class);
-    primitiveToWrapper.put(byte.class, Byte.class);
-    primitiveToWrapper.put(short.class, Short.class);
-    primitiveToWrapper.put(char.class, Character.class);
-    primitiveToWrapper.put(int.class, Integer.class);
-    primitiveToWrapper.put(long.class, Long.class);
-    primitiveToWrapper.put(float.class, Float.class);
-    primitiveToWrapper.put(double.class, Double.class);
-    primitiveToWrapper.put(void.class, Void.class);
-
-    wrapperToPrimitive.put(Boolean.class, boolean.class);
-    wrapperToPrimitive.put(Byte.class, byte.class);
-    wrapperToPrimitive.put(Short.class, short.class);
-    wrapperToPrimitive.put(Character.class, char.class);
-    wrapperToPrimitive.put(Integer.class, int.class);
-    wrapperToPrimitive.put(Long.class, long.class);
-    wrapperToPrimitive.put(Float.class, float.class);
-    wrapperToPrimitive.put(Double.class, double.class);
-    wrapperToPrimitive.put(Void.class, void.class);
-
-    primitiveToDescriptor.put(boolean.class, 'Z');
-    primitiveToDescriptor.put(byte.class, 'B');
-    primitiveToDescriptor.put(short.class, 'S');
-    primitiveToDescriptor.put(char.class, 'C');
-    primitiveToDescriptor.put(int.class, 'I');
-    primitiveToDescriptor.put(long.class, 'J');
-    primitiveToDescriptor.put(float.class, 'F');
-    primitiveToDescriptor.put(double.class, 'D');
-    primitiveToDescriptor.put(void.class, 'V');
-  }
 
   /**
    * Finds a pattern of opcodes and returns the first node of the matched pattern if found
@@ -147,15 +109,6 @@ public class ASMHelper {
         i < Math.abs(n) && node != null;
         ++i, node = n > 0 ? node.getNext() : node.getPrevious()) ;
     return node;
-  }
-
-  public static AbstractInsnNode findStart(AbstractInsnNode start) {
-    AbstractInsnNode next = start;
-    do {
-      if (next.getOpcode() != -1) return next;
-      else next = next.getNext();
-    } while (next != null);
-    return null;
   }
 
   public static String getClassData(ClassNode node) {
