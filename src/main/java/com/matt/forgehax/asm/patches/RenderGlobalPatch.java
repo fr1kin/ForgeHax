d9ccfb1c56468f3f49910ac26732f4852c59862c
babbaj <babbaj45@gmail.com>
2019-01-02 06:27:08 -0500

    Ass BlockHighlight mod

--- src/main/java/com/matt/forgehax/asm/patches/RenderGlobalPatch.java
+++ src/main/java/com/matt/forgehax/asm/patches/RenderGlobalPatch.java
@@ -3,6 +3,8 @@ package com.matt.forgehax.asm.patches;
 import static org.objectweb.asm.Opcodes.*;
 
 import com.matt.forgehax.asm.TypesHook;
+import com.matt.forgehax.asm.TypesHook.Methods;
+import com.matt.forgehax.asm.events.DrawBlockBoundingBoxEvent;
 import com.matt.forgehax.asm.utils.ASMHelper;
 import com.matt.forgehax.asm.utils.asmtype.ASMMethod;
 import com.matt.forgehax.asm.utils.transforming.ClassTransformer;
@@ -11,6 +13,7 @@ import com.matt.forgehax.asm.utils.transforming.MethodTransformer;
 import com.matt.forgehax.asm.utils.transforming.RegisterMethodTransformer;
 import java.util.Objects;
 import org.objectweb.asm.tree.*;
+import scala.tools.asm.Type;
 
 public class RenderGlobalPatch extends ClassTransformer {
   public RenderGlobalPatch() {
@@ -178,6 +181,63 @@ public class RenderGlobalPatch extends ClassTransformer {
       // iload should be below here
 
       main.instructions.insertBefore(node, insnList);
+    }
+  }
+
+  @RegisterMethodTransformer
+  private class DrawBoundingBox extends MethodTransformer {
+
+    @Override
+    public ASMMethod getMethod() {
+      return Methods.RenderGlobal_drawBoundingBox;
+    }
+
+    @Inject(description = "Add hook at the top of the method")
+    public void inject(MethodNode main) {
+      AbstractInsnNode start = main.instructions.getFirst();
+      AbstractInsnNode end = ASMHelper.findPattern(start, RETURN);
+
+      final int eventIndex =
+          ASMHelper.addNewLocalVariable(
+              main, "forgehax_event", Type.getDescriptor(DrawBlockBoundingBoxEvent.Pre.class));
+
+      InsnList pushArgs = new InsnList();
+      pushArgs.add(new VarInsnNode(FLOAD, 12));
+      pushArgs.add(new VarInsnNode(FLOAD, 13));
+      pushArgs.add(new VarInsnNode(FLOAD, 14));
+      pushArgs.add(new VarInsnNode(FLOAD, 15));
+
+      InsnList newEvent =
+          ASMHelper.newInstance(
+              Type.getInternalName(DrawBlockBoundingBoxEvent.Pre.class), "(FFFF)V", pushArgs);
+
+      final InsnList pre = new InsnList();
+      pre.add(newEvent);
+      pre.add(new VarInsnNode(ASTORE, eventIndex));
+      pre.add(new VarInsnNode(ALOAD, eventIndex));
+      pre.add(ASMHelper.call(INVOKESTATIC, TypesHook.Methods.ForgeHaxHooks_fireEvent_v));
+      pre.add(setColor(eventIndex, "red", 12));
+      pre.add(setColor(eventIndex, "green", 13));
+      pre.add(setColor(eventIndex, "blue", 14));
+      pre.add(setColor(eventIndex, "alpha", 15));
+
+      final InsnList post = new InsnList();
+      post.add(
+          ASMHelper.call(INVOKESTATIC, TypesHook.Methods.ForgeHaxHooks_onDrawBoundingBox_Post));
+
+      main.instructions.insert(start, pre);
+      main.instructions.insertBefore(end, post);
+    }
+
+    private InsnList setColor(int eventIndex, String field, int colorIndex) {
+      InsnList list = new InsnList();
+      list.add(new VarInsnNode(ALOAD, eventIndex));
+      list.add(
+          new FieldInsnNode(
+              GETFIELD, Type.getInternalName(DrawBlockBoundingBoxEvent.class), field, "F"));
+      list.add(new VarInsnNode(FSTORE, colorIndex));
+
+      return list;
     }
   }
 }
