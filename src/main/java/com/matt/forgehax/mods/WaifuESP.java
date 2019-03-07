b2db1e606ab1e96980fc7df9529c0b5eac031582
babbaj <babbaj45@gmail.com>
2019-03-06 21:20:24 -0500

    Fix WaifuESP

--- src/main/java/com/matt/forgehax/mods/WaifuESP.java
+++ src/main/java/com/matt/forgehax/mods/WaifuESP.java
@@ -7,18 +7,21 @@ import com.matt.forgehax.util.math.VectorUtils;
 import com.matt.forgehax.util.mod.Category;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
-import java.awt.image.BufferedImage;
-import java.io.BufferedInputStream;
-import java.io.File;
-import java.io.IOException;
+
+import java.io.*;
 import java.net.URL;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import javax.imageio.ImageIO;
+import java.util.Optional;
+
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.renderer.GlStateManager;
 import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.MissingTextureSprite;
 import net.minecraft.client.renderer.texture.NativeImage;
+import net.minecraft.client.renderer.texture.TextureUtil;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.util.ResourceLocation;
@@ -27,122 +30,129 @@ import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderPlayerEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.lwjgl.system.MemoryUtil;
 
 @RegisterMod
 public class WaifuESP extends ToggleMod {
-  public WaifuESP() {
-    super(Category.RENDER, "WaifuESP", false, "overlay cute animes over players");
-  }
-
-  public final Setting<Boolean> noRenderPlayers =
-      getCommandStub()
-          .builders()
-          .<Boolean>newSettingBuilder()
-          .name("noRenderPlayers")
-          .description("render other players")
-          .defaultTo(false)
-          .build();
-
-  // private final ResourceLocation waifu = new ResourceLocation("textures/forgehax/waifu1.png");
-  private ResourceLocation waifu;
-
-  private final String waifuUrl =
-      "https://raw.githubusercontent.com/fr1kin/ForgeHax/master/src/main/resources/assets/minecraft/textures/forgehax/waifu1.png";
-
-  private final Path waifuCache = Helper.getFileManager().getBaseResolve("cache/waifu.png");
-
-  private <T> BufferedImage getImage(T source, ThrowingFunction<T, BufferedImage> readFunction) {
-    try {
-      return readFunction.apply(source);
-    } catch (IOException ex) {
-      ex.printStackTrace();
-      return null;
+    public WaifuESP() {
+        super(Category.RENDER, "WaifuESP", false, "overlay cute animes over players");
     }
-  }
 
-  private boolean shouldDraw(EntityLivingBase entity) {
-    return (!entity.equals(MC.player)
-        && EntityUtils.isAlive(entity)
-        && EntityUtils.isValidEntity(entity)
-        && (EntityUtils.isPlayer(entity)));
-  }
+    public final Setting<Boolean> noRenderPlayers =
+        getCommandStub()
+            .builders()
+            .<Boolean>newSettingBuilder()
+            .name("noRenderPlayers")
+            .description("render other players")
+            .defaultTo(false)
+            .build();
 
-  @SubscribeEvent(priority = EventPriority.LOWEST)
-  public void onRenderGameOverlayEvent(RenderGameOverlayEvent.Text event) {
-    if (waifu == null) return;
+    // default to missing texture, will be scheduled to be set in onLoad()
+    private ResourceLocation waifu = MissingTextureSprite.getLocation();
 
-    for (Entity entity : MC.world.loadedEntityList) {
-      if (EntityUtils.isLiving(entity) && shouldDraw((EntityLivingBase) entity)) {
-        EntityLivingBase living = (EntityLivingBase) (entity);
-        Vec3d bottomVec = EntityUtils.getInterpolatedPos(living, event.getPartialTicks());
-        Vec3d topVec =
-            bottomVec.add(new Vec3d(0, (entity.getRenderBoundingBox().maxY - entity.posY), 0));
-        VectorUtils.ScreenPos top = VectorUtils._toScreen(topVec.x, topVec.y, topVec.z);
-        VectorUtils.ScreenPos bot = VectorUtils._toScreen(bottomVec.x, bottomVec.y, bottomVec.z);
-        if (top.isVisible || bot.isVisible) {
+    private final String waifuUrl =
+        "https://raw.githubusercontent.com/fr1kin/ForgeHax/master/src/main/resources/assets/minecraft/textures/forgehax/waifu1.png";
 
-          int height = (bot.y - top.y);
-          int width = height;
+    private final Path waifuCache = Helper.getFileManager().getBaseResolve("cache/waifu.png");
 
-          int x =
-              (int) (top.x - (width / 1.8)); // normally 2.0 but lowering it shifts it to the left
-          int y = top.y;
-
-          // draw waifu
-          MC.textureManager.bindTexture(waifu);
-
-          GlStateManager.color3f(255, 255, 255);
-          Gui.drawScaledCustomSizeModalRect(
-              x, y, 0, 0, width, height, width, height, width, height);
-        }
-      }
+    private boolean shouldDraw(EntityLivingBase entity) {
+        return (!entity.equals(MC.player)
+            && EntityUtils.isAlive(entity)
+            && EntityUtils.isValidEntity(entity)
+            && (EntityUtils.isPlayer(entity)));
     }
-  }
 
-  @SubscribeEvent
-  public void onRenderPlayer(RenderPlayerEvent.Pre event) {
-    if (noRenderPlayers.getAsBoolean() && !event.getEntity().equals(MC.player)) {
-      event.setCanceled(true);
-    }
-  }
+    @SubscribeEvent(priority = EventPriority.LOWEST)
+    public void onRenderGameOverlayEvent(RenderGameOverlayEvent.Text event) {
+        if (waifu == null) return;
 
-  @Override
-  public void onLoad() {
-    MC.addScheduledTask(
-        () -> {
-          try {
-            //TODO: fix
-            /*BufferedImage image;
-            NativeImage _image;
-            //NativeImage.read(new BufferedInputStream(Files.newInputStream(waifuCache)));
-            if (Files.exists(waifuCache)) { // TODO: download async
-              image = getImage(waifuCache, ImageIO::read); // from cache
-            } else {
-              image = getImage(new URL(waifuUrl), ImageIO::read); // from internet
-              if (image != null) {
-                try {
-                  ImageIO.write(image, "png", waifuCache.toFile());
-                } catch (IOException ex) {
-                  ex.printStackTrace();
+        for (Entity entity : MC.world.loadedEntityList) {
+            if (EntityUtils.isLiving(entity) && shouldDraw((EntityLivingBase) entity)) {
+                EntityLivingBase living = (EntityLivingBase) (entity);
+                Vec3d bottomVec = EntityUtils.getInterpolatedPos(living, event.getPartialTicks());
+                Vec3d topVec =
+                    bottomVec.add(new Vec3d(0, (entity.getRenderBoundingBox().maxY - entity.posY), 0));
+                VectorUtils.ScreenPos top = VectorUtils._toScreen(topVec.x, topVec.y, topVec.z);
+                VectorUtils.ScreenPos bot = VectorUtils._toScreen(bottomVec.x, bottomVec.y, bottomVec.z);
+                if (top.isVisible || bot.isVisible) {
+
+                    int height = (bot.y - top.y);
+                    int width = height;
+
+                    int x =
+                        (int) (top.x - (width / 1.8)); // normally 2.0 but lowering it shifts it to the left
+                    int y = top.y;
+
+                    // draw waifu
+                    MC.textureManager.bindTexture(waifu);
+
+                    GlStateManager.color3f(255, 255, 255);
+                    Gui.drawScaledCustomSizeModalRect(
+                        x, y, 0, 0, width, height, width, height, width, height);
                 }
-              }
             }
-            if (image == null) {
-              LOGGER.warn("Failed to download waifu image");
-              return;
-            }
+        }
+    }
 
-            DynamicTexture dynamicTexture = new DynamicTexture(image);
-            dynamicTexture.loadTexture(MC.getResourceManager());
-            waifu = MC.getTextureManager().getDynamicTextureLocation("WAIFU", dynamicTexture);*/
-          } catch (Exception e) {
-            e.printStackTrace();
-          }
+    @SubscribeEvent
+    public void onRenderPlayer(RenderPlayerEvent.Pre event) {
+        if (noRenderPlayers.getAsBoolean() && !event.getEntity().equals(MC.player)) {
+            event.setCanceled(true);
+        }
+    }
+
+    @Override
+    public void onLoad() { // lol
+        MC.addScheduledTask(() -> {
+            this.waifu = Optional.of(waifuCache)
+                .map(cache -> {
+                    try {
+                        return Files.exists(cache) ?
+                            new BufferedInputStream(Files.newInputStream(cache)) :
+                            new URL(waifuUrl).openStream();
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        return null;
+                    }
+                })
+                .map(stream -> {
+                    ByteBuffer byteBuffer = null;
+                    try {
+                        byteBuffer = TextureUtil.readToNativeBuffer(stream);
+                        if (!Files.exists(waifuCache)) {
+                            byteBuffer.rewind();
+                            saveWaifuToCache(byteBuffer);
+                        }
+                        byteBuffer.rewind();
+                        return NativeImage.read(byteBuffer);
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                        return null;
+                    } finally {
+                        if (byteBuffer != null)
+                            MemoryUtil.memFree(byteBuffer);
+                    }
+                })
+                .map(image -> {
+                    final DynamicTexture dynamicTexture = new DynamicTexture(image);
+                    final ResourceLocation waifuLocation = new ResourceLocation("forgehax_waifu");
+                    MC.getTextureManager().loadTexture(waifuLocation, dynamicTexture);
+                    return waifuLocation;
+                })
+                .orElse(MissingTextureSprite.getLocation());
         });
-  }
+    }
 
-  @FunctionalInterface
-  private interface ThrowingFunction<T, R> {
-    R apply(T obj) throws IOException;
-  }
+    private void saveWaifuToCache(ByteBuffer buffer) {
+        try {
+            FileChannel fileOut = new FileOutputStream(waifuCache.toFile()).getChannel();
+            fileOut.write(buffer);
+            fileOut.close();
+        } catch (IOException ex) {
+            LOGGER.warn("Failed to save waifu to cache: " + ex);
+            ex.printStackTrace();
+        }
+    }
+
+
 }
