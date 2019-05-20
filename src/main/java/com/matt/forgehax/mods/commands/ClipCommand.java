7e5444258046a0dc81b32839f7c065b8db6f3969
fr1kin <matt.fr1kin@gmail.com>
2019-05-20 17:57:12 -0500

    added forward, vclip commands + bug fixes to regular clip command

--- src/main/java/com/matt/forgehax/mods/commands/ClipCommand.java
+++ src/main/java/com/matt/forgehax/mods/commands/ClipCommand.java
@@ -1,8 +1,14 @@
 package com.matt.forgehax.mods.commands;
 
+import static com.matt.forgehax.Helper.getLocalPlayer;
 import static com.matt.forgehax.Helper.getNetworkManager;
+import static com.matt.forgehax.Helper.getRidingEntity;
+import static com.matt.forgehax.Helper.getRidingOrPlayer;
+import static com.matt.forgehax.Helper.getWorld;
+import static com.matt.forgehax.Helper.printWarning;
 
 import com.matt.forgehax.Helper;
+import com.matt.forgehax.util.SafeConverter;
 import com.matt.forgehax.util.command.Command;
 import com.matt.forgehax.util.command.CommandBuilders;
 import com.matt.forgehax.util.mod.CommandMod;
@@ -11,6 +17,7 @@ import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.Entity;
 import net.minecraft.network.play.client.CPacketPlayer;
 import net.minecraft.network.play.client.CPacketVehicleMove;
+import net.minecraft.util.math.Vec3d;
 
 /** Created by Babbaj on 4/12/2018. */
 @RegisterMod
@@ -21,7 +28,7 @@ public class ClipCommand extends CommandMod {
   }
 
   // teleport to absolute position
-  private void teleport(double x, double y, double z) {
+  private void setPosition(double x, double y, double z) {
     final Entity local = Helper.getRidingOrPlayer();
     local.setPositionAndUpdate(x, y, z);
     if (local instanceof EntityPlayerSP) {
@@ -34,9 +41,9 @@ public class ClipCommand extends CommandMod {
   }
 
   // teleport vertically by some offset
-  private void teleport(double yOffset) {
+  private void offsetY(double yOffset) {
     Entity local = Helper.getRidingOrPlayer();
-    teleport(local.posX, local.posY + yOffset, local.posZ);
+    setPosition(local.posX, local.posY + yOffset, local.posZ);
   }
 
   @RegisterCommand
@@ -50,15 +57,36 @@ public class ClipCommand extends CommandMod {
             data -> {
               try {
                 switch (data.getArgumentCount()) {
-                  case 1:
-                    teleport(Double.parseDouble(data.getArgumentAsString(0)));
+                  case 1: {
+                    final double y = Double.parseDouble(data.getArgumentAsString(0));
+                    MC.addScheduledTask(() -> {
+                      if (getWorld() == null || getLocalPlayer() == null)
+                        return;
+
+                      Entity local = getRidingOrPlayer();
+                      if (local == null)
+                        return;
+
+                      setPosition(0, local.posY + y, 0);
+                    });
                     break;
-                  case 3:
-                    teleport(
-                        Double.parseDouble(data.getArgumentAsString(0)),
-                        Double.parseDouble(data.getArgumentAsString(1)),
-                        Double.parseDouble(data.getArgumentAsString(2)));
+                  }
+                  case 3: {
+                    final double x = Double.parseDouble(data.getArgumentAsString(0));
+                    final double y = Double.parseDouble(data.getArgumentAsString(1));
+                    final double z = Double.parseDouble(data.getArgumentAsString(2));
+                    MC.addScheduledTask(() -> {
+                      if (getWorld() == null || getLocalPlayer() == null)
+                        return;
+
+                      Entity local = getRidingOrPlayer();
+                      if (local == null)
+                        return;
+
+                      setPosition(local.posX + x, local.posY + y, local.posZ + z);
+                    });
                     break;
+                  }
                   default:
                     Helper.printMessage("Invalid number of arguments: expected 1 or 3");
                 }
@@ -66,6 +94,43 @@ public class ClipCommand extends CommandMod {
                 Helper.printMessage("Failed to parse input");
               }
             })
+        .build();
+  }
+
+  @RegisterCommand
+  public Command vclip(CommandBuilders builders) {
+    return builders.newCommandBuilder()
+        .name("vclip")
+        .description("Vertical clip")
+        .requiredArgs(1)
+        .processor(data -> {
+          if(getWorld() == null || getLocalPlayer() == null) {
+            printWarning("Not in game");
+            return;
+          }
+          final double y = SafeConverter.toDouble(data.getArgumentAsString(0));
+          MC.addScheduledTask(() -> offsetY(y));
+        })
+        .build();
+  }
+
+  @RegisterCommand
+  public Command forward(CommandBuilders builders) {
+    return builders.newCommandBuilder()
+        .name("forward")
+        .description("Forward clip")
+        .requiredArgs(1)
+        .processor(data -> {
+          if(getWorld() == null || getLocalPlayer() == null) {
+            printWarning("Not in game");
+            return;
+          }
+          final double units = SafeConverter.toDouble(data.getArgumentAsString(0));
+          MC.addScheduledTask(() -> {
+            Vec3d dir = getLocalPlayer().getLookVec().normalize();
+            setPosition(getLocalPlayer().posX + (dir.x * units), getLocalPlayer().posY, getLocalPlayer().posZ + (dir.z * units));
+          });
+        })
         .build();
   }
 }
