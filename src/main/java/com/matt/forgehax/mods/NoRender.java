8d16674336ca48e03272c8028e614b38b27d3d84
thelampgod <virtual@riseup.net>
2019-08-06 09:24:02 +0200

    fix small issues (tabbing, spacing, etc.)
    this works for now. can be improved upon to add "hide" setting, but i couldn't figure out how to do that

--- src/main/java/com/matt/forgehax/mods/NoRender.java
+++ src/main/java/com/matt/forgehax/mods/NoRender.java
@@ -1,10 +1,11 @@
 package com.matt.forgehax.mods;
 
-import com.matt.forgehax.events.Render2DEvent;
 import com.matt.forgehax.util.mod.Category;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.item.EntityItem;
+import net.minecraftforge.client.event.RenderLivingEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import static com.matt.forgehax.Helper.getWorld;
@@ -17,17 +18,12 @@ public class NoRender extends ToggleMod {
     }
 
     @SubscribeEvent
-    public void onRender2D(Render2DEvent event){
-    getWorld()
-         .loadedEntityList
-         .stream()
-         .filter(EntityItem.class::isInstance)
-         .map(EntityItem.class::cast)
-         .filter(entity -> entity.ticksExisted > 1)
-         .forEach(
-                 entity -> {
-                     entity.setDead();
-         });
+    public void onRenderLiving(RenderLivingEvent.Pre event) {
+        getWorld()
+                .loadedEntityList
+                .stream()
+                .filter(EntityItem.class::isInstance)
+                .map(EntityItem.class::cast)
+                .forEach(Entity::setDead);
     }
 }
-
