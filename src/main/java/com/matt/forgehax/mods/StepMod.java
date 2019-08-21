a66db95418bdfa42652f1b5790ad06a60c990733
Logan Darklock <logandarklock@gmail.com>
2019-08-21 12:34:02 -0700

    Inline _newY.get()

--- src/main/java/com/matt/forgehax/mods/StepMod.java
+++ src/main/java/com/matt/forgehax/mods/StepMod.java
@@ -86,8 +86,7 @@ public class StepMod extends ToggleMod {
     List<AxisAlignedBB> collisionBoxes = player.world.getCollisionBoxes(player, range);
     AtomicReference<Double> _newY = new AtomicReference<>(0D);
     collisionBoxes.forEach(box -> _newY.set(Math.max(_newY.get(), box.maxY)));
-    double newY = _newY.get();
-    player.setPositionAndUpdate(player.posX, newY, player.posZ);
+    player.setPositionAndUpdate(player.posX, _newY.get(), player.posZ);
   }
 
   public void updateUnstep(EntityPlayer player) {
