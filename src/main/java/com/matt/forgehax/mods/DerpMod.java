df0625ea9babecb2ccfd03886eba26fad97e801e
Logan Darklock <logandarklock@gmail.com>
2019-08-21 22:20:52 -0700

    Derp mod beta

--- src/main/java/com/matt/forgehax/mods/DerpMod.java
+++ src/main/java/com/matt/forgehax/mods/DerpMod.java
@@ -1,14 +1,24 @@
 package com.matt.forgehax.mods;
 
 import com.matt.forgehax.asm.events.PacketEvent;
-import com.matt.forgehax.util.PacketHelper;
+import com.matt.forgehax.events.LocalPlayerUpdateEvent;
 import com.matt.forgehax.util.command.Setting;
 import com.matt.forgehax.util.mod.Category;
 import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.Packet;
+import net.minecraft.network.play.client.CPacketAnimation;
+import net.minecraft.network.play.client.CPacketEntityAction;
 import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.util.EnumHand;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+
+import java.util.Random;
+
+import static com.matt.forgehax.Helper.getLocalPlayer;
+import static com.matt.forgehax.util.PacketHelper.ignoreAndSend;
+import static com.matt.forgehax.util.PacketHelper.isIgnored;
 
 @RegisterMod
 public class DerpMod extends ToggleMod {
@@ -16,54 +26,197 @@ public class DerpMod extends ToggleMod {
     super(Category.MISC, "Derp", false, "Derp");
   }
 
-  enum DerpMode {
-    UPSIDE_DOWN
-  }
+  private float error;
+  private boolean sneaking;
 
-  private final Setting<DerpMode> mode =
-      getCommandStub()
-          .builders()
-          .<DerpMode>newSettingEnumBuilder()
-          .name("mode")
-          .description("Derp mode")
-          .defaultTo(DerpMode.UPSIDE_DOWN)
-          .build();
+  @Override
+  protected void onEnabled() {
+    super.onEnabled();
+
+    error = 0;
+
+    EntityPlayer player = getLocalPlayer();
+    sneaking = player != null && player.isSneaking();
+  }
 
   private final Setting<Float> randomSpeed =
       getCommandStub()
           .builders()
           .<Float>newSettingBuilder()
           .name("speed")
-          .description("Random derp speed")
+          .description("Approximate derps per tick")
+          .defaultTo(.5f)
+          .changed(__ -> error = 0)
+          .build();
+
+  private final Setting<Boolean> rotate =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("rotate")
+          .description("Randomly rotate head every derp")
+          .changed(change -> MC.addScheduledTask(() -> {
+            if (isEnabled() && change.getFrom() && !change.getTo()) rotateDisabled();
+          }))
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Float> rotateChance =
+      getCommandStub()
+          .builders()
+          .<Float>newSettingBuilder()
+          .name("rotate_chance")
+          .description("Chance to rotate for every derp")
+          .min(0f)
+          .max(1f)
           .defaultTo(1f)
           .build();
 
+  private final Setting<Boolean> sneak =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("sneak")
+          .description("Toggle sneak every derp")
+          .changed(change -> MC.addScheduledTask(() -> {
+            if (!isEnabled()) return;
+            if (change.getFrom() && !change.getTo()) sneakDisabled();
+            if (!change.getFrom() && change.getTo()) {
+              EntityPlayer player = getLocalPlayer();
+              sneaking = player != null && player.isSneaking();
+            }
+          }))
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Float> sneakChance =
+      getCommandStub()
+          .builders()
+          .<Float>newSettingBuilder()
+          .name("sneak_chance")
+          .description("Chance to sneak for every derp")
+          .min(0f)
+          .max(1f)
+          .defaultTo(1f)
+          .build();
+
+  private final Setting<Boolean> hit =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("hit")
+          .description("Randomly hit every derp")
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Float> hitChance =
+      getCommandStub()
+          .builders()
+          .<Float>newSettingBuilder()
+          .name("hit_chance")
+          .description("Chance to hit for every derp")
+          .min(0f)
+          .max(1f)
+          .defaultTo(.25f)
+          .build();
+
+  private final Setting<Float> hitPan =
+      getCommandStub()
+          .builders()
+          .<Float>newSettingBuilder()
+          .name("hit_pan")
+          .description("chance for any hit to be the off hand")
+          .min(0f)
+          .max(1f)
+          .defaultTo(.5f)
+          .build();
+
+  private void sendRotatePacket(EntityPlayer player, float pitch, float yaw) {
+    ignoreAndSend(new CPacketPlayer.Rotation(yaw, pitch, player.onGround));
+  }
+
+  private void rotateDisabled() {
+    EntityPlayer player = getLocalPlayer();
+    if (player == null) return;
+
+    sendRotatePacket(player, player.rotationPitch, player.rotationYawHead);
+  }
+
+  private CPacketEntityAction.Action actionFromSneaking(boolean sneaking) {
+    return sneaking
+        ? CPacketEntityAction.Action.START_SNEAKING
+        : CPacketEntityAction.Action.STOP_SNEAKING;
+  }
+
+  private void sendSneakPacket(EntityPlayer player, boolean sneaking) {
+    this.sneaking = sneaking;
+    ignoreAndSend(new CPacketEntityAction(player, actionFromSneaking(sneaking)));
+  }
+
+  private void sneakDisabled() {
+    EntityPlayer player = getLocalPlayer();
+    if (player == null) return;
+
+    sendSneakPacket(player, player.isSneaking());
+  }
+
+  @Override
+  protected void onDisabled() {
+    if (rotate.get()) rotateDisabled();
+    if (sneak.get()) sneakDisabled();
+  }
+
+  private void sendHitPacket(EnumHand hand) {
+    ignoreAndSend(new CPacketAnimation(hand));
+  }
+
+  private boolean chance(float chance) {
+    return chance == 1f || (chance != 0f && Math.random() <= chance);
+  }
+
+  @SubscribeEvent
+  public void onLocalPlayerUpdate(LocalPlayerUpdateEvent event) {
+    EntityPlayer player = (EntityPlayer) event.getEntityLiving();
+    if (player == null) return;
+    Random rng = player.getRNG();
+
+    error += randomSpeed.get();
+    int iter = (int) error;
+    error -= iter;
+
+    for (int i = 0; i < iter; i++) {
+      if (rotate.get() && chance(rotateChance.get()))
+        sendRotatePacket(player, rng.nextInt(180) - 90, rng.nextInt(360) - 180);
+
+      if (sneak.get() && chance(sneakChance.get()))
+        sendSneakPacket(player, !sneaking);
+
+      if (hit.get() && chance(hitChance.get()))
+        sendHitPacket(chance(hitPan.get()) ? EnumHand.OFF_HAND : EnumHand.MAIN_HAND);
+    }
+  }
+
   @SubscribeEvent
   public void onPacketSending(PacketEvent.Outgoing.Pre event) {
-    if (mode.get() != DerpMode.UPSIDE_DOWN) return;
-    if (PacketHelper.isIgnored(event.getPacket()) || !(event.getPacket() instanceof CPacketPlayer)) return;
+    Packet<?> packet = event.getPacket();
+    if (isIgnored(packet)) return;
 
-    CPacketPlayer packet = event.getPacket();
-    boolean isRotation = packet instanceof CPacketPlayer.Rotation || packet instanceof CPacketPlayer.PositionRotation;
-
-    if (isRotation) {
-      float newPitch = 360;
-      float newYaw = packet.getYaw(0) + 360;
-      boolean onGround = packet.isOnGround();
-      Packet<?> toSend;
+    if (rotate.get() && (packet instanceof CPacketPlayer.Rotation || packet instanceof CPacketPlayer.PositionRotation)) {
+      event.setCanceled(true);
 
       if (packet instanceof CPacketPlayer.PositionRotation) {
-        double x = packet.getX(0);
-        double y = packet.getY(0);
-        double z = packet.getZ(0);
-
-        toSend = new CPacketPlayer.PositionRotation(x, y, z, newYaw, newPitch, onGround);
-      } else {
-        toSend = new CPacketPlayer.Rotation(newYaw, newPitch, onGround);
+        ignoreAndSend(new CPacketPlayer.Position(
+            ((CPacketPlayer) packet).getX(0),
+            ((CPacketPlayer) packet).getY(0),
+            ((CPacketPlayer) packet).getZ(0),
+            ((CPacketPlayer) packet).isOnGround()
+        ));
       }
+    } else if (sneak.get() && packet instanceof CPacketEntityAction) {
+      CPacketEntityAction.Action action = ((CPacketEntityAction) packet).getAction();
 
-      PacketHelper.ignoreAndSend(toSend);
-      event.setCanceled(true);
+      if (action == CPacketEntityAction.Action.START_SNEAKING || action == CPacketEntityAction.Action.STOP_SNEAKING)
+        sneaking = action == CPacketEntityAction.Action.START_SNEAKING;
     }
   }
 }
