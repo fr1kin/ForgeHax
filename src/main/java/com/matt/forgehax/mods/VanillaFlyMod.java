7f10f06f9a1d006a5da6abc0c7a20576b4b54184
Logan Darklock <logandarklock@gmail.com>
2019-08-23 17:01:36 -0700

    AntiGround + FastReflection + new Fields

--- src/main/java/com/matt/forgehax/mods/VanillaFlyMod.java
+++ src/main/java/com/matt/forgehax/mods/VanillaFlyMod.java
@@ -1,6 +1,7 @@
 package com.matt.forgehax.mods;
 
 import com.matt.forgehax.asm.events.PacketEvent;
+import com.matt.forgehax.asm.reflection.FastReflection.Fields;
 import com.matt.forgehax.events.LocalPlayerUpdateEvent;
 import com.matt.forgehax.util.Switch.Handle;
 import com.matt.forgehax.util.command.Setting;
@@ -9,8 +10,8 @@ import com.matt.forgehax.util.mod.ToggleMod;
 import com.matt.forgehax.util.mod.loader.RegisterMod;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.play.client.CPacketPlayer;
+import net.minecraft.network.play.server.SPacketPlayerPosLook;
 import net.minecraft.util.math.AxisAlignedBB;
-import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 import java.util.List;
@@ -32,6 +33,16 @@ public class VanillaFlyMod extends ToggleMod {
           .name("spoof")
           .description("make the server think we are on the ground while flying")
           .defaultTo(false)
+          .build();
+
+  @SuppressWarnings("WeakerAccess")
+  public final Setting<Boolean> antiGround =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("antiground")
+          .description("attempts to prevent the server from teleporting us to the ground")
+          .defaultTo(true)
           .build();
 
   @SuppressWarnings("WeakerAccess")
@@ -82,16 +93,48 @@ public class VanillaFlyMod extends ToggleMod {
       return;
 
     CPacketPlayer packet = event.getPacket();
-
-    if (!(boolean) ObfuscationReflectionHelper.getPrivateValue(CPacketPlayer.class, packet, "moving", "field_149480_h", "g"))
-      return;
+    if (!Fields.CPacketPlayer_moving.get(packet)) return;
 
     AxisAlignedBB range = player.getEntityBoundingBox().expand(0, -player.posY, 0).contract(0, -player.height, 0);
     List<AxisAlignedBB> collisionBoxes = player.world.getCollisionBoxes(player, range);
     AtomicReference<Double> newHeight = new AtomicReference<>(0D);
     collisionBoxes.forEach(box -> newHeight.set(Math.max(newHeight.get(), box.maxY)));
 
-    ObfuscationReflectionHelper.setPrivateValue(CPacketPlayer.class, packet, newHeight.get(), "y", "field_149477_b", "b");
-    ObfuscationReflectionHelper.setPrivateValue(CPacketPlayer.class, packet, true, "onGround", "field_149474_g", "f");
+    Fields.CPacketPlayer_y.set(packet, newHeight.get());
+    Fields.CPacketPlayer_onGround.set(packet, true);
+  }
+
+  @SubscribeEvent
+  public void onPacketRecieving(PacketEvent.Incoming.Pre event) {
+    EntityPlayer player = getLocalPlayer();
+    if (isNull(player)) return;
+
+    if (!antiGround.get() || !(event.getPacket() instanceof SPacketPlayerPosLook) || !player.capabilities.isFlying)
+      return;
+
+    SPacketPlayerPosLook packet = event.getPacket();
+
+    double oldY = player.posY;
+    player.setPosition(
+        Fields.SPacketPlayer_x.get(packet),
+        Fields.SPacketPlayer_y.get(packet),
+        Fields.SPacketPlayer_z.get(packet)
+    );
+
+    /*
+     * This needs a little explanation, as I had a little trouble wrapping my head around it myself.
+     * Basically, we're trying to find a new position that's as close to the original height as possible.
+     * That way, if you're, for example, spoofing and the server rubberbands you back, you don't go back to the ground.
+     * This tries to find the lowest block above the spot the server teleported you to, and teleport you right to that.
+     * If the lowest block is below where you were before, it just teleports you to where you were before.
+     * This allows VanillaFly to be slightly more usable on servers like Constantiam that like to teleport you in place
+     * to hopefully disable fly hacks. Well, sorry guys, this fly hack is smarter than that.
+     */
+    AxisAlignedBB range = player.getEntityBoundingBox().expand(0, 256 - player.height - player.posY, 0).contract(0, player.height, 0);
+    List<AxisAlignedBB> collisionBoxes = player.world.getCollisionBoxes(player, range);
+    AtomicReference<Double> newY = new AtomicReference<>(256D);
+    collisionBoxes.forEach(box -> newY.set(Math.min(newY.get(), box.minY - player.height)));
+
+    Fields.SPacketPlayer_y.set(packet, Math.min(oldY, newY.get()));
   }
 }
