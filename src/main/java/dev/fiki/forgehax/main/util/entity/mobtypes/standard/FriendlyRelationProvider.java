5be5a534faafd6fd6129c87a6faeb56436b89c42
mattcc <matt.fr1kin@gmail.com>
2020-02-20 22:31:04 -0600

    Shulkers and Shulker bullets are properly targeted now

--- src/main/java/dev/fiki/forgehax/main/util/entity/mobtypes/standard/FriendlyRelationProvider.java
+++ src/main/java/dev/fiki/forgehax/main/util/entity/mobtypes/standard/FriendlyRelationProvider.java
@@ -1,17 +1,27 @@
 package dev.fiki.forgehax.main.util.entity.mobtypes.standard;
 
+import com.google.common.collect.Sets;
 import dev.fiki.forgehax.main.util.common.PriorityEnum;
 import dev.fiki.forgehax.main.util.entity.mobtypes.EntityRelationProvider;
 import dev.fiki.forgehax.main.util.entity.mobtypes.RelationState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.merchant.villager.VillagerEntity;
 import net.minecraft.entity.passive.GolemEntity;
+
+import java.util.Set;
 
 /**
  * Created on 6/27/2017 by fr1kin
  */
 public class FriendlyRelationProvider extends EntityRelationProvider<Entity> {
+  private final Set<EntityType> friendlyTypes = Sets.newHashSet();
+
+  {
+    friendlyTypes.add(EntityType.IRON_GOLEM);
+    friendlyTypes.add(EntityType.VILLAGER);
+  }
 
   @Override
   protected PriorityEnum getPriority() {
@@ -20,11 +30,8 @@ public class FriendlyRelationProvider extends EntityRelationProvider<Entity> {
 
   @Override
   public boolean isProviderFor(Entity entity) {
-    return EntityClassification.CREATURE.equals(entity.getClassification(false))
-        || EntityClassification.WATER_CREATURE.equals(entity.getClassification(false))
-        || EntityClassification.AMBIENT.equals(entity.getClassification(false))
-        || entity instanceof VillagerEntity
-        || entity instanceof GolemEntity;
+    return entity.getClassification(false).getPeacefulCreature()
+        || friendlyTypes.contains(entity.getType());
   }
 
   @Override
--- src/main/java/dev/fiki/forgehax/main/util/entity/mobtypes/standard/HostileRelationProvider.java
+++ src/main/java/dev/fiki/forgehax/main/util/entity/mobtypes/standard/HostileRelationProvider.java
@@ -1,15 +1,26 @@
 package dev.fiki.forgehax.main.util.entity.mobtypes.standard;
 
+import com.google.common.collect.Sets;
 import dev.fiki.forgehax.main.util.common.PriorityEnum;
 import dev.fiki.forgehax.main.util.entity.mobtypes.EntityRelationProvider;
 import dev.fiki.forgehax.main.util.entity.mobtypes.RelationState;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityClassification;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.monster.ShulkerEntity;
+
+import java.util.Set;
 
 /**
  * Created on 6/27/2017 by fr1kin
  */
 public class HostileRelationProvider extends EntityRelationProvider<Entity> {
+  private final Set<EntityType> hostileTypes = Sets.newHashSet();
+
+  {
+    hostileTypes.add(EntityType.SHULKER);
+    hostileTypes.add(EntityType.SHULKER_BULLET);
+  }
 
   @Override
   protected PriorityEnum getPriority() {
@@ -18,7 +29,8 @@ public class HostileRelationProvider extends EntityRelationProvider<Entity> {
 
   @Override
   public boolean isProviderFor(Entity entity) {
-    return EntityClassification.MONSTER.equals(entity.getClassification(false));
+    return !entity.getClassification(false).getPeacefulCreature()
+        || hostileTypes.contains(entity.getType());
   }
 
   @Override
