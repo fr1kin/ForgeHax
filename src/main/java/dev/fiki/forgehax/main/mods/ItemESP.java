583dff1c66f8e888cafc84e7bd90b213159a8807
mattcc <matt.fr1kin@gmail.com>
2020-02-20 22:31:28 -0600

    Fixed ItemESP

--- src/main/java/dev/fiki/forgehax/main/mods/ItemESP.java
+++ src/main/java/dev/fiki/forgehax/main/mods/ItemESP.java
@@ -1,10 +1,13 @@
 package dev.fiki.forgehax.main.mods;
 
+import com.mojang.blaze3d.matrix.MatrixStack;
 import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
 import dev.fiki.forgehax.main.Common;
 import dev.fiki.forgehax.main.events.Render2DEvent;
+import dev.fiki.forgehax.main.util.cmd.settings.ColorSetting;
 import dev.fiki.forgehax.main.util.cmd.settings.DoubleSetting;
+import dev.fiki.forgehax.main.util.cmd.settings.FloatSetting;
 import dev.fiki.forgehax.main.util.color.Colors;
 import dev.fiki.forgehax.main.util.draw.SurfaceHelper;
 import dev.fiki.forgehax.main.util.entity.EntityUtils;
@@ -13,6 +16,7 @@ import dev.fiki.forgehax.main.util.math.VectorUtils;
 import dev.fiki.forgehax.main.util.mod.Category;
 import dev.fiki.forgehax.main.util.mod.ToggleMod;
 import dev.fiki.forgehax.main.util.mod.loader.RegisterMod;
+import net.minecraft.client.renderer.IRenderTypeBuffer;
 import net.minecraft.entity.item.ItemEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.math.Vec3d;
@@ -20,16 +24,23 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 
 import java.util.stream.StreamSupport;
 
+import static dev.fiki.forgehax.main.Common.getBufferProvider;
 import static dev.fiki.forgehax.main.Common.worldEntities;
 
 @RegisterMod
 public class ItemESP extends ToggleMod {
 
-  public final DoubleSetting scale = newDoubleSetting()
+  private final FloatSetting scale = newFloatSetting()
       .name("scale")
       .description("Scaling for text")
-      .defaultTo(1.D)
-      .min(0.D)
+      .defaultTo(1.f)
+      .min(0.f)
+      .build();
+
+  private final ColorSetting color = newColorSetting()
+      .name("color")
+      .description("Color of the item esp font")
+      .defaultTo(Colors.WHITE)
       .build();
 
   public ItemESP() {
@@ -38,16 +49,10 @@ public class ItemESP extends ToggleMod {
 
   @SubscribeEvent
   public void onRender2D(final Render2DEvent event) {
-    RenderSystem.enableBlend();
-    RenderSystem.blendFuncSeparate(
-        GlStateManager.SourceFactor.SRC_ALPHA.param,
-        GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA.param,
-        GlStateManager.SourceFactor.ONE.param,
-        GlStateManager.DestFactor.ZERO.param);
-    RenderSystem.enableTexture();
-    RenderSystem.disableDepthTest();
+    final float scale = this.scale.getValue() == 0 ? 1.f : this.scale.getValue();
 
-    final double scale = this.scale.getValue() == 0 ? 1.D : this.scale.getValue();
+    final IRenderTypeBuffer.Impl buffers = getBufferProvider().getBufferSource();
+    final MatrixStack stack = new MatrixStack();
 
     worldEntities()
         .filter(ItemEntity.class::isInstance)
@@ -64,27 +69,25 @@ public class ItemESP extends ToggleMod {
             return;
           }
 
-          double offX = bot.getX() - top.getX();
-          double offY = bot.getY() - top.getY();
+          stack.push();
+          stack.translate(top.getX(), bot.getY(), 0.f);
 
-          RenderSystem.pushMatrix();
-          RenderSystem.translated(top.getX() - (offX / 2.D), bot.getY(), 0);
+          ItemStack itemStack = entity.getItem();
+          String text = itemStack.getDisplayName().getUnformattedComponentText()
+              + (itemStack.isStackable() ? (" x" + itemStack.getCount()) : "");
 
-          ItemStack stack = entity.getItem();
-          String text =
-              stack.getDisplayName() + (stack.isStackable() ? (" x" + stack.getCount()) : "");
+          stack.scale(scale, scale, 0.f);
+          stack.translate(
+              -SurfaceHelper.getStringWidth(text) / 2.f,
+              -SurfaceHelper.getStringHeight() / 2.f,
+              0.f);
 
-          SurfaceHelper.drawTextShadow(
-              text,
-              (int) (offX / 2.D - SurfaceHelper.getStringWidth(text) * scale / 2.D),
-              -(int) (offY - SurfaceHelper.getStringHeight() * scale / 2.D) - 1,
-              Colors.WHITE.toBuffer(),
-              scale);
+          SurfaceHelper.renderString(buffers, stack.getLast().getPositionMatrix(),
+              text, 0, 0, color.getValue(), true);
 
-          RenderSystem.popMatrix();
+          stack.pop();
         });
 
-    RenderSystem.enableDepthTest();
-    RenderSystem.disableBlend();
+    buffers.finish();
   }
 }
