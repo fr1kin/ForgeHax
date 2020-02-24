a373ae7df7731bc11cd9c15eb550093f5a5633d1
mattcc <matt.fr1kin@gmail.com>
2020-02-24 17:46:36 -0600

    AutoFishMod will reel in if bobber is on non liquid block

--- src/main/java/dev/fiki/forgehax/main/mods/AutoFishMod.java
+++ src/main/java/dev/fiki/forgehax/main/mods/AutoFishMod.java
@@ -1,15 +1,15 @@
 package dev.fiki.forgehax.main.mods;
 
 import dev.fiki.forgehax.common.events.packet.PacketInboundEvent;
-import dev.fiki.forgehax.main.Common;
+import dev.fiki.forgehax.main.events.LocalPlayerUpdateEvent;
 import dev.fiki.forgehax.main.util.cmd.settings.DoubleSetting;
 import dev.fiki.forgehax.main.util.cmd.settings.IntegerSetting;
-import dev.fiki.forgehax.main.util.reflection.FastReflection;
-import dev.fiki.forgehax.main.events.LocalPlayerUpdateEvent;
 import dev.fiki.forgehax.main.util.mod.Category;
 import dev.fiki.forgehax.main.util.mod.ToggleMod;
 import dev.fiki.forgehax.main.util.mod.loader.RegisterMod;
+import dev.fiki.forgehax.main.util.reflection.FastReflection;
 import net.minecraft.client.entity.player.ClientPlayerEntity;
+import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.network.play.server.SPlaySoundEffectPacket;
@@ -17,6 +17,8 @@ import net.minecraft.util.SoundEvents;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.client.event.InputEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+
+import static dev.fiki.forgehax.main.Common.*;
 
 /**
  * Created on 9/2/2016 by fr1kin
@@ -29,23 +31,26 @@ public class AutoFishMod extends ToggleMod {
 
   private boolean previouslyHadRodEquipped = false;
 
-  public final IntegerSetting casting_delay = newIntegerSetting()
+  public final IntegerSetting castingDelay = newIntegerSetting()
       .name("casting-delay")
       .description("Number of ticks to wait after casting the rod to attempt a recast")
+      .min(0)
       .defaultTo(20)
       .build();
 
-  public final DoubleSetting max_sound_distance = newDoubleSetting()
+  public final DoubleSetting maxSoundDistance = newDoubleSetting()
       .name("max-sound-distance")
       .description("Maximum distance between the splash sound and hook entity allowed"
           + " (set to 0 to disable this feature)")
+      .min(0)
       .defaultTo(2.D)
       .build();
 
-  public final IntegerSetting fail_safe_time = newIntegerSetting()
-      .name("fail-safe-time")
+  public final IntegerSetting recastDelay = newIntegerSetting()
+      .name("recast-delay")
       .description("Maximum amount of time (in ticks) allowed until the hook is pulled in"
           + "(set to 0 to disable this feature)")
+      .min(0)
       .defaultTo(600)
       .build();
 
@@ -54,20 +59,20 @@ public class AutoFishMod extends ToggleMod {
   }
 
   private boolean isCorrectSplashPacket(SPlaySoundEffectPacket packet) {
-    ClientPlayerEntity me = Common.getLocalPlayer();
+    ClientPlayerEntity me = getLocalPlayer();
     return packet.getSound().equals(SoundEvents.ENTITY_FISHING_BOBBER_SPLASH)
         && (me != null
         && me.fishingBobber != null
-        && (max_sound_distance.getValue() == 0
-        || // disables this check
-        (me.fishingBobber.getPositionVector()
-            .distanceTo(new Vec3d(packet.getX(), packet.getY(), packet.getZ())) <= max_sound_distance.getValue())));
+        && (maxSoundDistance.getValue() == 0 // disables this check
+        || (me.fishingBobber.getPositionVector()
+            .distanceTo(new Vec3d(packet.getX(), packet.getY(), packet.getZ())) <= maxSoundDistance.getValue())));
   }
 
   private void rightClick() {
     if (ticksCastDelay <= 0) { // to prevent the fishing rod from being spammed when in hand
-      FastReflection.Methods.Minecraft_rightClickMouse.invoke(Common.MC);
-      ticksCastDelay = casting_delay.getValue();
+      FastReflection.Methods.Minecraft_rightClickMouse.invoke(MC);
+      ticksCastDelay = castingDelay.getValue();
+      ticksHookDeployed = 0;
     }
   }
 
@@ -84,12 +89,12 @@ public class AutoFishMod extends ToggleMod {
 
   @SubscribeEvent
   public void onUpdate(LocalPlayerUpdateEvent event) {
-    ClientPlayerEntity me = Common.getLocalPlayer();
+    ClientPlayerEntity me = getLocalPlayer();
     ItemStack heldStack = me.getHeldItemMainhand();
 
     // update tick delay if hook is deployed
-    if (ticksCastDelay > casting_delay.getValue()) {
-      ticksCastDelay = casting_delay.getValue(); // greater than current delay, set to the current delay
+    if (ticksCastDelay > castingDelay.getValue()) {
+      ticksCastDelay = castingDelay.getValue(); // greater than current delay, set to the current delay
     } else if (ticksCastDelay > 0) {
       --ticksCastDelay;
     }
@@ -97,7 +102,7 @@ public class AutoFishMod extends ToggleMod {
     // check if player is holding a fishing rod
     if (Items.FISHING_ROD.equals(heldStack.getItem())) { // item being held is a fishing rod {
       if (!previouslyHadRodEquipped) {
-        ticksCastDelay = casting_delay.getValue();
+        ticksCastDelay = castingDelay.getValue();
         previouslyHadRodEquipped = true;
       } else if (me.fishingBobber == null) { // no hook is deployed
         // cast hook
@@ -106,9 +111,16 @@ public class AutoFishMod extends ToggleMod {
         // increment the number of ticks that the hook entity has existed
         ++ticksHookDeployed;
 
-        if (fail_safe_time.getValue() != 0 && (ticksHookDeployed > fail_safe_time.getValue())) {
+        FishingBobberEntity bobber = me.fishingBobber;
+        boolean notInWater = false;
+
+        // check if the bobber is not moving at all
+        if(bobber.getMotion().subtract(0, bobber.getMotion().getY(), 0).lengthSquared() == 0) {
+          notInWater = !getWorld().getBlockState(bobber.getPosition()).getMaterial().isLiquid();
+        }
+
+        if (notInWater || (recastDelay.getValue() != 0 && (ticksHookDeployed > recastDelay.getValue()))) {
           rightClick(); // reel in hook if the fail safe time has passed
-          resetLocals();
         }
       }
     } else {
@@ -118,8 +130,8 @@ public class AutoFishMod extends ToggleMod {
 
   @SubscribeEvent
   public void onMouseEvent(InputEvent.MouseInputEvent event) {
-    if (Common.getGameSettings().keyBindUseItem.isKeyDown() && ticksHookDeployed > 0) {
-      ticksCastDelay = casting_delay.getValue();
+    if (getGameSettings().keyBindUseItem.isKeyDown() && ticksHookDeployed > 0) {
+      ticksCastDelay = castingDelay.getValue();
     }
   }
 
