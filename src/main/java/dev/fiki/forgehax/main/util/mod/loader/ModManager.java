e51389bdef032fda112ebbba5e54ae9b80e233f9
mattcc <matt.fr1kin@gmail.com>
2020-02-25 15:31:17 -0600

    Added concurrent mod loading/reloading support

--- src/main/java/dev/fiki/forgehax/main/util/mod/loader/ModManager.java
+++ src/main/java/dev/fiki/forgehax/main/util/mod/loader/ModManager.java
@@ -1,39 +1,47 @@
 package dev.fiki.forgehax.main.util.mod.loader;
 
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableSortedSet;
+import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
-import dev.fiki.forgehax.main.util.cmd.ICommand;
-import dev.fiki.forgehax.main.util.mod.AbstractMod;
 import dev.fiki.forgehax.main.util.FileHelper;
 import dev.fiki.forgehax.main.util.Immutables;
 import dev.fiki.forgehax.main.util.classloader.AbstractClassLoader;
 import dev.fiki.forgehax.main.util.classloader.ClassLoaderHelper;
 import dev.fiki.forgehax.main.util.classloader.CustomClassLoaders;
+import dev.fiki.forgehax.main.util.cmd.ICommand;
+import dev.fiki.forgehax.main.util.mod.AbstractMod;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.lang.annotation.Annotation;
 import java.nio.file.FileSystem;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.Set;
+import java.util.*;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Consumer;
 import java.util.stream.Stream;
-import javax.annotation.Nullable;
 
-import static dev.fiki.forgehax.main.Common.*;
+import static dev.fiki.forgehax.main.Common.getLogger;
 
 /**
  * Created on 5/16/2017 by fr1kin
  */
 public class ModManager extends AbstractClassLoader<AbstractMod> {
   private final Set<Class<? extends AbstractMod>> classes = Sets.newHashSet();
-  private final Set<AbstractMod> active =
-      Sets.newTreeSet(Comparator.comparing(ICommand::getName, String.CASE_INSENSITIVE_ORDER));
+
+  private final Comparator<AbstractMod> comparator =
+      Comparator.comparing(ICommand::getName, String.CASE_INSENSITIVE_ORDER);
+  private final Set<AbstractMod> active = Sets.newTreeSet(comparator);
+
+  private Map<Class<? extends AbstractMod>, AbstractMod> CLASS_TO_INSTANCE = Maps.newHashMap();
+  private Map<String, AbstractMod> NAME_TO_INSTANCE = Maps.newTreeMap(String::compareToIgnoreCase);
+
+  private volatile Set<AbstractMod> readOnlyActiveMods = ImmutableSortedSet.copyOf(comparator, active);
+
+  private ReadWriteLock lock = new ReentrantReadWriteLock();
 
   public boolean searchPackage(String packageDir) {
     try {
@@ -93,12 +101,12 @@ public class ModManager extends AbstractClassLoader<AbstractMod> {
 
   private Stream<Class<? extends AbstractMod>> unloadedClasses() {
     return classes.stream()
-        .filter(clazz -> active.stream().noneMatch(mod -> mod.getClass().equals(clazz)));
+        .filter(clazz -> !get(clazz).isPresent());
   }
 
   private Stream<Class<? extends AbstractMod>> loadedClasses() {
     return classes.stream()
-        .filter(clazz -> active.stream().anyMatch(mod -> mod.getClass().equals(clazz)));
+        .filter(clazz -> get(clazz).isPresent());
   }
 
   public Collection<Class<? extends AbstractMod>> getUnloadedClasses() {
@@ -110,31 +118,78 @@ public class ModManager extends AbstractClassLoader<AbstractMod> {
   }
 
   public Collection<AbstractMod> getMods() {
-    return Collections.unmodifiableCollection(active);
+    return readOnlyActiveMods;
   }
 
   public Optional<? extends AbstractMod> get(final String modName) {
-    return active.stream().filter(mod -> mod.getName().equalsIgnoreCase(modName)).findFirst();
+    lock.readLock().lock();
+    try {
+      return Optional.ofNullable(NAME_TO_INSTANCE.get(modName));
+    } finally {
+      lock.readLock().unlock();
+    }
   }
 
   @SuppressWarnings("unchecked")
   public <T extends AbstractMod> Optional<T> get(final Class<T> clazz) {
-    return active.stream()
-        .filter(mod -> Objects.equals(clazz, mod.getClass()))
-        .map(mod -> (T) mod)
-        .findFirst();
+    lock.readLock().lock();
+    try {
+      return Optional.ofNullable((T) CLASS_TO_INSTANCE.get(clazz));
+    } finally {
+      lock.readLock().unlock();
+    }
+  }
+
+  public boolean isModEnabled(Class<? extends AbstractMod> clazz) {
+    return get(clazz)
+        .map(AbstractMod::isEnabled)
+        .orElse(false);
   }
 
   public void load(Class<? extends AbstractMod> clazz) {
-    unloadedClasses().filter(clazz::equals).findFirst().ifPresent(this::_load);
+    unloadedClasses()
+        .filter(clazz::equals)
+        .findFirst()
+        .ifPresent(this::_load);
+  }
+
+  private boolean addActiveMod(AbstractMod mod) {
+    Objects.requireNonNull(mod, "Mod is null!");
+    lock.writeLock().lock();
+    try {
+      if (active.add(mod)) {
+        CLASS_TO_INSTANCE.put(mod.getClass(), mod);
+        NAME_TO_INSTANCE.put(mod.getName(), mod);
+        readOnlyActiveMods = ImmutableSortedSet.copyOf(comparator, active);
+        return true;
+      }
+    } finally {
+      lock.writeLock().unlock();
+    }
+    return false;
+  }
+
+  private boolean removeActiveMod(AbstractMod mod) {
+    Objects.requireNonNull(mod, "Mod is null!");
+    lock.writeLock().lock();
+    try {
+      if (active.remove(mod)) {
+        CLASS_TO_INSTANCE.remove(mod.getClass());
+        NAME_TO_INSTANCE.remove(mod.getName());
+        readOnlyActiveMods = ImmutableSortedSet.copyOf(comparator, active);
+        return true;
+      }
+    } finally {
+      lock.writeLock().unlock();
+    }
+    return false;
   }
 
   private void _load(Class<? extends AbstractMod> clazz) {
-    AbstractMod mod = loadClass(clazz);
-    if (mod != null && active.add(mod)) {
+    if (addActiveMod(loadClass(clazz))) {
       getLogger().debug("Loading class {}", clazz.getSimpleName());
     } else {
-      getLogger().warn("Mod {} is null!", clazz.getSimpleName());
+      getLogger().warn("Failed to add mod {}. Possibly already exists?", clazz.getSimpleName());
     }
   }
 
@@ -143,7 +198,7 @@ public class ModManager extends AbstractClassLoader<AbstractMod> {
   }
 
   public void unload(AbstractMod mod) {
-    if (active.remove(mod)) {
+    if (removeActiveMod(mod)) {
       mod.unload();
     }
   }
