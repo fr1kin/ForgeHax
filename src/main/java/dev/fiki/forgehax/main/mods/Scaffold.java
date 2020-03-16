5a05e26cb996e2d909c1d38fb3c32bbed6eef8be
mattcc <matt.fr1kin@gmail.com>
2020-03-16 18:46:01 -0500

    Scaffold is now much faster

--- src/main/java/dev/fiki/forgehax/main/mods/Scaffold.java
+++ src/main/java/dev/fiki/forgehax/main/mods/Scaffold.java
@@ -1,19 +1,7 @@
 package dev.fiki.forgehax.main.mods;
 
-import static dev.fiki.forgehax.main.Common.*;
-import static net.minecraft.network.play.client.CEntityActionPacket.*;
-
 import com.google.common.collect.Lists;
-import dev.fiki.forgehax.main.Common;
-import dev.fiki.forgehax.main.util.cmd.settings.IntegerSetting;
-import dev.fiki.forgehax.main.util.common.PriorityEnum;
-import dev.fiki.forgehax.main.util.entity.EntityUtils;
-import dev.fiki.forgehax.main.util.entity.LocalPlayerInventory;
-import dev.fiki.forgehax.main.util.entity.LocalPlayerUtils;
-import dev.fiki.forgehax.main.util.math.Angle;
-import dev.fiki.forgehax.main.util.mod.Category;
-import dev.fiki.forgehax.main.util.mod.ToggleMod;
-import dev.fiki.forgehax.main.util.mod.loader.RegisterMod;
+import dev.fiki.forgehax.main.events.RenderEvent;
 import dev.fiki.forgehax.main.mods.managers.PositionRotationManager;
 import dev.fiki.forgehax.main.mods.managers.PositionRotationManager.RotationState.Local;
 import dev.fiki.forgehax.main.mods.services.HotbarSelectionService.ResetFunction;
@@ -21,15 +9,21 @@ import dev.fiki.forgehax.main.util.BlockHelper;
 import dev.fiki.forgehax.main.util.BlockHelper.BlockTraceInfo;
 import dev.fiki.forgehax.main.util.PacketHelper;
 import dev.fiki.forgehax.main.util.Utils;
-
-import java.util.Comparator;
-import java.util.EnumSet;
-import java.util.Objects;
-import java.util.Optional;
-import java.util.stream.StreamSupport;
-
+import dev.fiki.forgehax.main.util.cmd.settings.BooleanSetting;
+import dev.fiki.forgehax.main.util.cmd.settings.DoubleSetting;
+import dev.fiki.forgehax.main.util.cmd.settings.IntegerSetting;
+import dev.fiki.forgehax.main.util.color.Colors;
+import dev.fiki.forgehax.main.util.common.PriorityEnum;
+import dev.fiki.forgehax.main.util.draw.BufferBuilderEx;
+import dev.fiki.forgehax.main.util.draw.GeometryMasks;
+import dev.fiki.forgehax.main.util.entity.LocalPlayerInventory;
+import dev.fiki.forgehax.main.util.entity.LocalPlayerUtils;
+import dev.fiki.forgehax.main.util.mod.Category;
+import dev.fiki.forgehax.main.util.mod.ToggleMod;
+import dev.fiki.forgehax.main.util.mod.loader.RegisterMod;
 import dev.fiki.forgehax.main.util.reflection.FastReflection;
 import net.minecraft.block.Block;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.item.BlockItem;
 import net.minecraft.network.play.client.CAnimateHandPacket;
 import net.minecraft.network.play.client.CEntityActionPacket;
@@ -38,6 +32,16 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
 import net.minecraft.util.math.Vec3d;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import org.lwjgl.opengl.GL11;
+
+import java.util.Comparator;
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Optional;
+
+import static dev.fiki.forgehax.main.Common.*;
+import static net.minecraft.network.play.client.CEntityActionPacket.Action;
 
 @RegisterMod
 public class Scaffold extends ToggleMod implements PositionRotationManager.MovementUpdateListener {
@@ -49,9 +53,28 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
       .defaultTo(4)
       .build();
 
+  private final DoubleSetting placeDistance = newDoubleSetting()
+      .name("place-distance")
+      .description("Place block before side is fully visible. Use 0 to disable")
+      .defaultTo(0.1d)
+      .min(0.d)
+      .build();
+
+  private final BooleanSetting motionPrediction = newBooleanSetting()
+      .name("motion-prediction")
+      .description("Will try and predict the future position to place a block")
+      .defaultTo(true)
+      .build();
+
+  private final BooleanSetting debug = newBooleanSetting()
+      .name("debug")
+      .description("Shows debug render info")
+      .defaultTo(false)
+      .build();
+
   private int tickCount = 0;
-  private boolean placing = false;
-  private Angle previousAngles = Angle.ZERO;
+  private BlockPos currentTarget = null;
+  private boolean predicted = false;
 
   public Scaffold() {
     super(Category.PLAYER, "Scaffold", false, "Place blocks under yourself");
@@ -60,6 +83,9 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
   @Override
   protected void onEnabled() {
     PositionRotationManager.getManager().register(this, PriorityEnum.HIGHEST);
+    currentTarget = null;
+    predicted = false;
+    tickCount = 0;
   }
 
   @Override
@@ -68,21 +94,57 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
   }
 
   @Override
+  public String getDebugDisplayText() {
+    return super.getDebugDisplayText()
+        + " ["
+        + "ticks = " + tickCount + ", "
+        + "vel = " + (getLocalPlayer() == null
+          ? "(0.00, 0.00)"
+          : String.format("(%.2f, %.2f)", getLocalPlayer().getMotion().getX(), getLocalPlayer().getMotion().getZ()))
+        + ", "
+        + "pos = " + (currentTarget == null
+          ? "(0, 0, 0)"
+          : String.format("(%d, %d, %d)", currentTarget.getX(), currentTarget.getY(), currentTarget.getZ()))
+        + "]";
+  }
+
+  @Override
   public void onLocalPlayerMovementUpdate(Local state) {
-    if (placing) {
-      ++tickCount;
-    }
+    currentTarget = null;
 
-    if (LocalPlayerUtils.getVelocity().normalize().length() > 1.D && placing) {
-      state.setServerAngles(previousAngles);
-    } else {
-      placing = false;
-      tickCount = 0;
-    }
-
+    Vec3d directionVector = Vec3d.ZERO;
     BlockPos below = getLocalPlayer().getPosition().down();
+    if(BlockHelper.isBlockReplaceable(below)) {
+      currentTarget = below;
+      predicted = true;
+    } else if(motionPrediction.isEnabled()) {
+      // try and get the block the player will be over
+      Vec3d motion = getLocalPlayer().getMotion();
+      // ignore y motion
+      Vec3d vel = new Vec3d(motion.getX(), 0.d, motion.getZ()).normalize();
 
-    if (!getWorld().getBlockState(below).getMaterial().isReplaceable()) {
+      // must be moving
+      if(vel.lengthSquared() > 0.d) {
+        double modX, modZ;
+        if(Math.abs(vel.getX()) > Math.abs(vel.getZ())) {
+          modX = vel.getX() < 0.d ? -1.d : 1.d;
+          modZ = 0.d;
+        } else {
+          modX = 0.d;
+          modZ = vel.getZ() < 0.d ? -1.d : 1.d;
+        }
+
+        directionVector = new Vec3d(modX, 0.d, modZ);
+        BlockPos forward = below.add(directionVector.getX(), directionVector.getY(), directionVector.getZ());
+        if(BlockHelper.isBlockReplaceable(forward)) {
+          currentTarget = forward;
+          predicted = true;
+        }
+      }
+    }
+
+    // no valid block found
+    if(currentTarget == null) {
       return;
     }
 
@@ -99,14 +161,19 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
       return;
     }
 
-    final Vec3d eyes = getLocalPlayer().getEyePosition(1.f);
-    final Vec3d dir = getLocalPlayer().getLookVec().normalize();
+    boolean isPredictedTarget = directionVector.lengthSquared() > 0.d;
+
+    final Vec3d realEyes = getLocalPlayer().getEyePosition(1.f);
+    final Vec3d eyes = isPredictedTarget
+        ? realEyes.add(directionVector)
+        : realEyes;
+    final Vec3d dir = LocalPlayerUtils.getServerDirectionVector();
 
     BlockTraceInfo trace =
-        Optional.ofNullable(BlockHelper.getPlaceableBlockSideTrace(eyes, dir, below))
+        Optional.ofNullable(BlockHelper.getPlaceableBlockSideTrace(eyes, dir, currentTarget))
             .filter(tr -> tr.isPlaceable(items))
             .orElseGet(() -> horizontal.stream()
-                .map(below::offset)
+                .map(currentTarget::offset)
                 .filter(BlockHelper::isBlockReplaceable)
                 .map(bp -> BlockHelper.getPlaceableBlockSideTrace(eyes, dir, bp))
                 .filter(Objects::nonNull)
@@ -118,8 +185,22 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
       return;
     }
 
+    currentTarget = trace.getPos();
+    predicted = false;
     Vec3d hit = trace.getHitVec();
-    state.setServerAngles(previousAngles = Utils.getLookAtAngles(hit));
+    state.setServerAngles(Utils.getLookAtAngles(hit));
+
+    // cannot place yet because of delay
+    if(tickCount++ < delay.intValue()) {
+      return;
+    }
+
+    // block is too far away or not visible
+    if(placeDistance.doubleValue() > 0.d
+        ? getLocalPlayer().getPositionVec().distanceTo(trace.getCenterPos()) < placeDistance.doubleValue()
+        : isPredictedTarget) {
+      return;
+    }
 
     final BlockTraceInfo tr = trace;
     state.invokeLater(rs -> {
@@ -155,8 +236,26 @@ public class Scaffold extends ToggleMod implements PositionRotationManager.Movem
       func.revert();
 
       FastReflection.Fields.Minecraft_rightClickDelayTimer.set(MC, delay.getValue());
-      placing = true;
       tickCount = 0;
     });
+  }
+
+  @SubscribeEvent
+  public void onRender(RenderEvent event) {
+    final BlockPos current = currentTarget;
+    if(debug.isEnabled() && current != null) {
+      BufferBuilderEx buffer = event.getBuffer();
+
+      buffer.beginLines(DefaultVertexFormats.POSITION_COLOR);
+      buffer.setTranslation(event.getProjectedPos().scale(-1));
+
+      Vec3d pos = new Vec3d(current);
+      buffer.putOutlinedCuboid(pos, pos.add(1.D, 1.D, 1.D), GeometryMasks.Line.ALL,
+          predicted ? Colors.YELLOW : Colors.ORANGE);
+
+      GL11.glEnable(GL11.GL_LINE_SMOOTH);
+      buffer.draw();
+      GL11.glDisable(GL11.GL_LINE_SMOOTH);
+    }
   }
 }
