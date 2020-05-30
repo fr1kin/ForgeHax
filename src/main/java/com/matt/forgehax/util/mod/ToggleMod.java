530f23dd02f954a2119d6150e911ab38c75e27c4
tonio-cartonio <62854391+tonio-cartonio@users.noreply.github.com>
2020-05-30 20:01:14 +0200

    Whops I'm dumb that was the constructor
    Both attributes hidden and enabled get a command in the ToggleMod constructor

--- src/main/java/com/matt/forgehax/util/mod/ToggleMod.java
+++ src/main/java/com/matt/forgehax/util/mod/ToggleMod.java
@@ -5,10 +5,10 @@ import com.matt.forgehax.util.command.StubBuilder;
 import com.matt.forgehax.util.command.callbacks.CallbackData;
 
 public class ToggleMod extends BaseMod {
-  
+
   private final Setting<Boolean> enabled;
   private final Setting<Boolean> hidden;
-  
+
   public ToggleMod(Category category, String modName, boolean defaultValue, String description) {
     super(category, modName, description);
     this.enabled =
@@ -28,10 +28,6 @@ public class ToggleMod extends BaseMod {
                   }
                 })
             .build();
-  }
-  
-  public HideMod(Category category, String modName, boolean defaultValue, String description) {
-    super(category, modName, description);
     this.hidden =
         getCommandStub()
             .builders()
@@ -40,8 +36,7 @@ public class ToggleMod extends BaseMod {
             .description("Hides the mod from modlist")
             .defaultTo(defaultValue)
             .changed(
-                cb -> { // value is set after callback is ran 🤡
-                  // do not call anything that might infinitely call this callback
+                cb -> {
                   if (cb.getTo()) {
                     start();
                   } else {
@@ -50,7 +45,7 @@ public class ToggleMod extends BaseMod {
                 })
             .build();
   }
-  
+
   /**
    * Toggle mod to be on/off
    */
@@ -61,57 +56,57 @@ public class ToggleMod extends BaseMod {
       enable();
     }
   }
-  
+
   @Override
   public void enable() {
     enabled.set(true);
   }
-  
+
   @Override
   public void disable() {
     enabled.set(false);
   }
-  
+
   /**
    * Toggle mod to be displayed or not
    */
-  public final void toggle() {
+  public final void display() {
     if (isHidden()) {
       hide();
     } else {
       show();
     }
   }
-  
+
   @Override
   public void hide() {
     hidden.set(true);
   }
-  
+
   @Override
   public void show() {
     hidden.set(false);
   }
-  
+
   @Override
   protected StubBuilder buildStubCommand(StubBuilder builder) {
     return builder.kpressed(this::onBindPressed).kdown(this::onBindKeyDown).bind();
   }
-  
+
   @Override
   public String getDebugDisplayText() {
     return super.getDebugDisplayText();
   }
- 
+
   /**
    * Check if the mod is currently shown
    */
-  
+
   @Override
   public boolean isHidden() {
     return hidden.get();
   }
-  
+
   /**
    * Check if the mod is currently enabled
    */
@@ -119,23 +114,23 @@ public class ToggleMod extends BaseMod {
   public final boolean isEnabled() {
     return enabled.get();
   }
-  
+
   @Override
   protected void onLoad() {
   }
-  
+
   @Override
   protected void onUnload() {
   }
-  
+
   @Override
   protected void onEnabled() {
   }
-  
+
   @Override
   protected void onDisabled() {
   }
-  
+
   /**
    * Toggles the mod
    */
@@ -143,7 +138,7 @@ public class ToggleMod extends BaseMod {
   public void onBindPressed(CallbackData cb) {
     toggle();
   }
-  
+
   @Override
   protected void onBindKeyDown(CallbackData cb) {
   }
