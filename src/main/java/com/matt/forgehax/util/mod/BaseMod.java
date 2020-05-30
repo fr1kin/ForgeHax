c77fc7f8b12c9ac21627674dbc9ee2d9e068c54d
tonio-cartonio <62854391+tonio-cartonio@users.noreply.github.com>
2020-05-30 20:18:25 +0200

    Restored whitespace
    In my vimrc I strip trailing whitespace. I restored it because it was making my merge harder to read

--- src/main/java/com/matt/forgehax/util/mod/BaseMod.java
+++ src/main/java/com/matt/forgehax/util/mod/BaseMod.java
@@ -1,7 +1,7 @@
 package com.matt.forgehax.util.mod;
-
+  
 import static com.matt.forgehax.Helper.getGlobalCommand;
-
+  
 import com.matt.forgehax.Globals;
 import com.matt.forgehax.util.command.Command;
 import com.matt.forgehax.util.command.ExecuteData;
@@ -12,21 +12,21 @@ import java.util.Collection;
 import java.util.Collections;
 import joptsimple.internal.Strings;
 import net.minecraftforge.common.MinecraftForge;
-
+  
 public abstract class BaseMod implements Globals {
-
+  
   // name of the mod
   private final String modName;
   // description of mod
   private final String modDescription;
   // category of the mod
   private final Category category;
-
+  
   protected final Command stubCommand;
-
+  
   // is the mod registered on the forge bus?
   private boolean registered = false;
-
+  
   public BaseMod(Category category, String name, String desc) {
     this.modName = name;
     this.modDescription = desc;
@@ -41,11 +41,11 @@ public abstract class BaseMod implements Globals {
                 .processor(this::onProcessCommand))
             .build();
   }
-
+  
   public BaseMod(Category category, String name) {
     this(category, name, Strings.EMPTY);
   }
-
+  
   /**
    * Load the mod
    */
@@ -58,7 +58,7 @@ public abstract class BaseMod implements Globals {
     }
     onLoad();
   }
-
+  
   /**
    * Unload the mod
    */
@@ -71,7 +71,7 @@ public abstract class BaseMod implements Globals {
       stubCommand.leaveParent();
     }
   }
-
+  
   /**
    * Enables the mod
    */
@@ -81,65 +81,65 @@ public abstract class BaseMod implements Globals {
       LOGGER.info(String.format("%s enabled", getModName()));
     }
   }
-
+  
   protected final void stop() {
     if (unregister()) {
       onDisabled();
       LOGGER.info(String.format("%s disabled", getModName()));
     }
   }
-
+  
   public void enable() {
     start();
   }
-
+  
   public void disable() {
     stop();
   }
-
+  
   public void hide() {
     return;
   }
-
+  
   public void show() {
     return;
   }
-
+  
   /**
    * Get the categories name
    */
   public final String getModName() {
     return modName;
   }
-
+  
   /**
    * Get mod description
    */
   public final String getModDescription() {
     return modDescription;
   }
-
+  
   /**
    * Get mod category
    */
   public Category getModCategory() {
     return category;
   }
-
+  
   /**
    * The main mod command
    */
   public Command getCommandStub() {
     return stubCommand;
   }
-
+  
   /**
    * Check if mod is currently registered
    */
   public final boolean isRegistered() {
     return registered;
   }
-
+  
   /**
    * Register event to forge bus
    */
@@ -152,7 +152,7 @@ public abstract class BaseMod implements Globals {
       return false;
     }
   }
-
+  
   /**
    * Unregister event on forge bus
    */
@@ -165,11 +165,11 @@ public abstract class BaseMod implements Globals {
       return false;
     }
   }
-
+  
   protected StubBuilder buildStubCommand(StubBuilder builder) {
     return builder;
   }
-
+  
   @SuppressWarnings("unchecked")
   public final <T extends Command> T getCommand(String commandName) {
     try {
@@ -178,11 +178,11 @@ public abstract class BaseMod implements Globals {
       return null;
     }
   }
-
+  
   public final Setting<?> getSetting(String settingName) {
     return getCommand(settingName);
   }
-
+  
   public final Collection<Command> getCommands() {
     if (stubCommand != null) {
       return stubCommand.getChildren();
@@ -190,17 +190,17 @@ public abstract class BaseMod implements Globals {
       return Collections.emptyList();
     }
   }
-
+  
   /**
    * Check if the mod is hidden DEFAULT: true
    */
   public abstract boolean isHidden();
-
+  
   /**
    * Check if the mod is enabled
    */
   public abstract boolean isEnabled();
-
+  
   private void writeChildren(
       StringBuilder builder, Command command, final boolean deep, final String append) {
     command
@@ -220,7 +220,7 @@ public abstract class BaseMod implements Globals {
               }
             });
   }
-
+  
   protected void onProcessCommand(ExecuteData data) {
     if (data.getArgumentCount() == 0 && !data.options().hasOptions()) {
       final StringBuilder builder = new StringBuilder();
@@ -228,45 +228,45 @@ public abstract class BaseMod implements Globals {
       data.write(builder.toString());
     }
   }
-
+  
   /**
    * Called when the mod is loaded
    */
   protected abstract void onLoad();
-
+  
   /**
    * Called when unloaded
    */
   protected abstract void onUnload();
-
+  
   /**
    * Called when the mod is enabled
    */
   protected abstract void onEnabled();
-
+  
   /**
    * Called when the mod is disabled
    */
   protected abstract void onDisabled();
-
+  
   /**
    * Called when the bind is initially pressed
    */
   protected abstract void onBindPressed(CallbackData cb);
-
+  
   /**
    * Called while the bind key is pressed down
    */
   protected abstract void onBindKeyDown(CallbackData cb);
-
+  
   public String getDisplayText() {
     return getModName();
   }
-
+  
   public String getDebugDisplayText() {
     return getDisplayText();
   }
-
+  
   @Override
   public String toString() {
     return getModName() + ": " + getModDescription();
