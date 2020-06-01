9f621bf6ecefac6a968088dada4034e6b156a8d0
OverFloyd <claudio.dario320@gmail.com>
2020-06-01 20:47:50 +0200

    +InfoDisplay

---
+++ src/main/java/com/matt/forgehax/mods/infooverlay/Saturation.java
@@ -0,0 +1,26 @@
+package com.matt.forgehax.mods.infooverlay;
+
+import com.matt.forgehax.util.MathUtil;
+import com.matt.forgehax.util.mod.Category;
+import com.matt.forgehax.util.mod.ToggleMod;
+import com.matt.forgehax.util.mod.loader.RegisterMod;
+
+@RegisterMod
+public class Saturation extends ToggleMod {
+
+  public Saturation() {
+    super(Category.GUI, "Saturation", true, "Shows your saturation level");
+  }
+
+  @Override
+  public boolean isInfoDisplayElement() {
+    return true;
+  }
+
+  /*@Override
+  public boolean isVisible() { return false; }*/
+
+  public String getInfoDisplayText() {
+    return "Saturation: " + String.format("%s", MathUtil.round(MC.player.getFoodStats().getSaturationLevel(), 1));
+  }
+}
