ef112bebc6b9d1a6027b87d6364e003b25cad26a
OverFloyd <example@example.com>
2020-06-14 21:51:19 +0200

    +DisconnectCommand

--- src/main/java/com/matt/forgehax/mods/commands/HelpCommand.java
+++ src/main/java/com/matt/forgehax/mods/commands/HelpCommand.java
@@ -5,6 +5,7 @@ import static com.matt.forgehax.Helper.getLocalPlayer;
 import static com.matt.forgehax.Helper.getModManager;
 
 import com.google.common.util.concurrent.FutureCallback;
+import com.matt.forgehax.Helper;
 import com.matt.forgehax.util.command.Command;
 import com.matt.forgehax.util.command.CommandBuilders;
 import com.matt.forgehax.util.console.ConsoleIO;
@@ -17,6 +18,7 @@ import java.util.Arrays;
 import java.util.List;
 import javax.annotation.Nullable;
 import joptsimple.internal.Strings;
+import net.minecraft.util.text.TextComponentString;
 
 /**
  * Created on 6/1/2017 by fr1kin
@@ -33,7 +35,7 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("save")
-      .description("Save all configurations")
+      .description("Save all configurations.")
       .processor(data -> getGlobalCommand().serializeAll())
       .build();
   }
@@ -43,15 +45,14 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("help")
-      .description("Help text for mod syntax and command list")
+      .description("Help text for mod syntax and command list.")
       .processor(
         data -> {
-          final StringBuilder build = new StringBuilder();
-          build.append("Type \".search <optional: containing string>\" for list of mods\n");
-          build.append("Use -? or --help after command to see command options\n");
-          build.append("See the FAQ for details\n");
-          build.append("https://github.com/fr1kin/ForgeHax#faq");
-          data.write(build.toString());
+          String build = "Type \".search <optional: containing string>\" for list of mods\n" +
+              "Use -? or --help after command to see command options\n" +
+              "See the FAQ for details\n" +
+              "https://github.com/fr1kin/ForgeHax#faq";
+          data.write(build);
           data.markSuccess();
         })
       .build();
@@ -62,11 +63,11 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("search")
-      .description("Lists all the mods or all the mods containing the given argument")
+      .description("Lists all the mods or all the mods containing the given argument.")
       .options(
         parser -> {
-          parser.acceptsAll(Arrays.asList("details", "d"), "Gives description");
-          parser.acceptsAll(Arrays.asList("hidden", "h"), "Show hidden mods");
+          parser.acceptsAll(Arrays.asList("details", "d"), "Gives description.");
+          parser.acceptsAll(Arrays.asList("hidden", "h"), "Show hidden mods.");
         })
       .processor(
         data -> {
@@ -104,11 +105,10 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("history")
-      .description("Lists name history of given player")
+      .description("Lists name history of given player.")
       .processor(
         data -> {
           data.requiredArguments(1);
-          final StringBuilder build = new StringBuilder();
           final String arg = data.getArgumentAsString(0);
           final int indents = ConsoleIO.getIndents();
           PlayerInfoHelper.registerWithCallback(
@@ -123,16 +123,16 @@ public class HelpCommand extends CommandMod {
                 ConsoleIO.setIndents(indents);
                 if (result.isOfflinePlayer()) {
                   ConsoleIO.write(
-                    String.format("\"%s\" is not a registered username", result.getName()));
+                    String.format("\"%s\" is not a registered username.", result.getName()));
                 } else {
                   if (result.getNameHistory().size() > 1) {
                     ConsoleIO.write(
                       String.format(
-                        "%s's name history (newest-oldest): %s",
+                        "%s's name history (newest-oldest): %s.",
                         result.getName(), result.getNameHistoryAsString()));
                   } else {
                     ConsoleIO.write(
-                      String.format("%s has never changed their name", result.getName()));
+                      String.format("%s has never changed their name.", result.getName()));
                   }
                 }
                 ConsoleIO.setIndents(previousIndents);
@@ -142,7 +142,7 @@ public class HelpCommand extends CommandMod {
               public void onFailure(Throwable t) {
               }
             });
-          data.write(build.toString());
+          data.write("");
           data.markSuccess();
         })
       .build();
@@ -153,7 +153,7 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("loaded")
-      .description("Loaded plugin list")
+      .description("Loaded plugin list.")
       .processor(
         data -> {
           final StringBuilder build = new StringBuilder();
@@ -179,7 +179,7 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("online")
-      .description("List of online players. Optionally with an argument to match")
+      .description("List of online players. Optionally with an argument to match.")
       .processor(
         data -> {
           List<PlayerInfo> players = PlayerInfoHelper.getOnlinePlayers();
@@ -218,14 +218,14 @@ public class HelpCommand extends CommandMod {
     return builder
       .newCommandBuilder()
       .name("respawn")
-      .description("Send respawn packet")
+      .description("Send respawn packet.")
       .processor(
         data -> {
           if (getLocalPlayer() != null) {
             getLocalPlayer().respawnPlayer();
             data.write("Respawn packet sent");
           } else {
-            data.write("Failed to send respawn packet (player is null)");
+            data.write("Failed to send respawn packet (player is null).");
           }
         })
       .build();
@@ -236,11 +236,38 @@ public class HelpCommand extends CommandMod {
     return builders
       .newCommandBuilder()
       .name("clear")
-      .description("Clears chat")
+      .description("Clears chat.")
       .options(p -> p.acceptsAll(Arrays.asList("all", "a"), "Also clear sent message history"))
       .processor(d -> MC.addScheduledTask(
         () -> MC.ingameGUI.getChatGUI().clearChatMessages(d.hasOption("all")))
       )
       .build();
+  }
+
+  /**
+   * Created by OverFloyd, june 14 2020
+   */
+  @RegisterCommand
+  public Command disconnect(CommandBuilders builders) {
+    return builders
+        .newCommandBuilder()
+        .name("dc")
+        .description("Disconnects from the server.")
+        .processor(
+            data -> {
+              if (MC.world == null || MC.player == null) {
+                Helper.printError("Not in game.");
+                return;
+              }
+
+              if (MC.isSingleplayer()) {
+                Helper.printInform("Disconnect command only works in multiplayer.");
+                return;
+              }
+
+              MC.player.connection.getNetworkManager().closeChannel(new TextComponentString("Disconnected by disconnect command."));
+            }
+        )
+        .build();
   }
 }
