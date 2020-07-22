1bf36def836420aee3ae0daef269aba82f6b373d
IronException <mail-fuer-friedolin@web.de>
2020-07-22 10:16:27 +0200

    improve spawnerEsp?

--- src/main/java/com/matt/forgehax/mods/SpawnerEspMod.java
+++ src/main/java/com/matt/forgehax/mods/SpawnerEspMod.java
@@ -28,13 +28,15 @@ public class SpawnerEspMod extends ToggleMod {
   
   @SubscribeEvent
   public void onRender(RenderEvent event) {
+    if(getWorld() == null) {
+      return;
+    }
     event.getBuffer().begin(GL11.GL_LINES, DefaultVertexFormats.POSITION_COLOR);
     
     for (TileEntity tileEntity : getWorld().loadedTileEntityList) {
       if (tileEntity instanceof TileEntityMobSpawner) {
-        BlockPos pos = tileEntity.getPos();
         GeometryTessellator.drawCuboid(
-            event.getBuffer(), pos, GeometryMasks.Line.ALL, Colors.RED.toBuffer());
+            event.getBuffer(), tileEntity.getPos(), GeometryMasks.Line.ALL, Colors.RED.toBuffer());
       }
     }
     
