34d0342f65ad2f0c9e654185c013c1371fb0d87a
mattcc <matt.fr1kin@gmail.com>
2020-12-09 16:28:15 -0600

    Fix ModLocator for newer versions of forge

--- src/core/java/dev/fiki/forgehax/asm/utils/ForgehaxModLocator.java
+++ src/core/java/dev/fiki/forgehax/asm/utils/ForgehaxModLocator.java
@@ -2,8 +2,11 @@ package dev.fiki.forgehax.asm.utils;
 
 import net.minecraftforge.fml.loading.moddiscovery.AbstractJarFileLocator;
 import net.minecraftforge.fml.loading.moddiscovery.ModFile;
+import net.minecraftforge.fml.loading.moddiscovery.ModFileParser;
 import net.minecraftforge.forgespi.locating.IModFile;
+import net.minecraftforge.forgespi.locating.IModLocator;
 
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 import java.util.Map;
@@ -18,9 +21,19 @@ public class ForgehaxModLocator extends AbstractJarFileLocator {
   public List<IModFile> scanMods() {
     return EZ.getJarPath()
         .map(p -> {
-          final ModFile modFile = new ModFile(p, this);
+          ModFile modFile;
+          try {
+            // for newer versions of forge
+            modFile = new ModFile(p, this, ModFileParser::modsTomlParser);
+          } catch (Throwable t) {
+            // for old versions of forge
+            try {
+              modFile = ModFile.class.getConstructor(Path.class, IModLocator.class).newInstance(p, this);
+            } catch (Throwable tt) {
+              throw new Error("Unable to build ModFile instance. No valid constructor found!", tt);
+            }
+          }
           this.modJars.compute(modFile, (mf, fs) -> this.createFileSystem(mf));
-
           return Collections.singletonList((IModFile) modFile);
         })
         .orElse(Collections.emptyList());
