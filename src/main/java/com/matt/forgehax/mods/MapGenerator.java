343beb42cde7adca8cc7379ada0a58b07f5ae333
orsond <orsondmc@gmail.com>
2021-01-22 19:41:05 +1100

    Add StashFinder

---
+++ src/main/java/com/matt/forgehax/mods/MapGenerator.java
@@ -0,0 +1,242 @@
+package com.matt.forgehax.mods;
+
+import com.google.common.collect.HashMultiset;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Multiset;
+import com.google.common.collect.Multisets;
+import com.matt.forgehax.Helper;
+import com.matt.forgehax.asm.reflection.FastReflection;
+import net.minecraft.block.BlockDirt;
+import net.minecraft.block.BlockStone;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.ITextureObject;
+import net.minecraft.init.Blocks;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.world.World;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.storage.MapData;
+import scala.Int;
+
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.util.Map;
+
+import static com.matt.forgehax.Globals.MC;
+import static com.matt.forgehax.util.ImageUtils.createResizedCopy;
+
+public class MapGenerator {
+
+  public static BufferedImage render(World worldIn, int posX, int posZ) {
+    MapData data = updateMapData(worldIn, posX, posZ);
+    return render(data, 512);
+  }
+
+  private static MapData updateMapData(World worldIn, int posX, int posZ) {
+    MapData data = new MapData("cute");
+    if (worldIn.provider.getDimension() == data.dimension)
+    {
+      int i = 1 << data.scale;
+      int j = data.xCenter;
+      int k = data.zCenter;
+      int l = MathHelper.floor(posX - (double)j) / i + 64;
+      int i1 = MathHelper.floor(posZ - (double)k) / i + 64;
+      int j1 = 128 / i;
+
+      if (worldIn.provider.isNether())
+      {
+        j1 /= 2;
+      }
+
+      boolean flag = false;
+
+      for (int k1 = l - j1 + 1; k1 < l + j1; ++k1)
+      {
+//        if ((k1 & 15) == (mapdata$mapinfo.step & 15) || flag)
+        if ((k1 & 15) == (1 & 15) || flag)
+        {
+          flag = false;
+          double d0 = 0.0D;
+
+          for (int l1 = i1 - j1 - 1; l1 < i1 + j1; ++l1)
+          {
+            if (k1 >= 0 && l1 >= -1 && k1 < 128 && l1 < 128)
+            {
+              int i2 = k1 - l;
+              int j2 = l1 - i1;
+              boolean flag1 = i2 * i2 + j2 * j2 > (j1 - 2) * (j1 - 2);
+              int k2 = (j / i + k1 - 64) * i;
+              int l2 = (k / i + l1 - 64) * i;
+              Multiset<MapColor> multiset = HashMultiset.<MapColor>create();
+              Chunk chunk = worldIn.getChunkFromBlockCoords(new BlockPos(k2, 0, l2));
+
+              if (!chunk.isEmpty())
+              {
+                int i3 = k2 & 15;
+                int j3 = l2 & 15;
+                int k3 = 0;
+                double d1 = 0.0D;
+
+                if (worldIn.provider.isNether())
+                {
+                  int l3 = k2 + l2 * 231871;
+                  l3 = l3 * l3 * 31287121 + l3 * 11;
+
+                  if ((l3 >> 20 & 1) == 0)
+                  {
+                    multiset.add(Blocks.DIRT.getDefaultState().withProperty(BlockDirt.VARIANT, BlockDirt.DirtType.DIRT).getMapColor(worldIn, BlockPos.ORIGIN), 10);
+                  }
+                  else
+                  {
+                    multiset.add(Blocks.STONE.getDefaultState().withProperty(BlockStone.VARIANT, BlockStone.EnumType.STONE).getMapColor(worldIn, BlockPos.ORIGIN), 100);
+                  }
+
+                  d1 = 100.0D;
+                }
+                else
+                {
+                  BlockPos.MutableBlockPos blockpos$mutableblockpos = new BlockPos.MutableBlockPos();
+
+                  for (int i4 = 0; i4 < i; ++i4)
+                  {
+                    for (int j4 = 0; j4 < i; ++j4)
+                    {
+                      int k4 = chunk.getHeightValue(i4 + i3, j4 + j3) + 1;
+                      IBlockState iblockstate = Blocks.AIR.getDefaultState();
+
+                      if (k4 <= 1)
+                      {
+                        iblockstate = Blocks.BEDROCK.getDefaultState();
+                      }
+                      else
+                      {
+                        label175:
+                        {
+                          while (true)
+                          {
+                            --k4;
+                            iblockstate = chunk.getBlockState(i4 + i3, k4, j4 + j3);
+                            blockpos$mutableblockpos.setPos((chunk.x << 4) + i4 + i3, k4, (chunk.z << 4) + j4 + j3);
+
+                            if (iblockstate.getMapColor(worldIn, blockpos$mutableblockpos) != MapColor.AIR || k4 <= 0)
+                            {
+                              break;
+                            }
+                          }
+
+                          if (k4 > 0 && iblockstate.getMaterial().isLiquid())
+                          {
+                            int l4 = k4 - 1;
+
+                            while (true)
+                            {
+                              IBlockState iblockstate1 = chunk.getBlockState(i4 + i3, l4--, j4 + j3);
+                              ++k3;
+
+                              if (l4 <= 0 || !iblockstate1.getMaterial().isLiquid())
+                              {
+                                break label175;
+                              }
+                            }
+                          }
+                        }
+                      }
+
+                      d1 += (double)k4 / (double)(i * i);
+                      multiset.add(iblockstate.getMapColor(worldIn, blockpos$mutableblockpos));
+                    }
+                  }
+                }
+
+                k3 = k3 / (i * i);
+                double d2 = (d1 - d0) * 4.0D / (double)(i + 4) + ((double)(k1 + l1 & 1) - 0.5D) * 0.4D;
+                int i5 = 1;
+
+                if (d2 > 0.6D)
+                {
+                  i5 = 2;
+                }
+
+                if (d2 < -0.6D)
+                {
+                  i5 = 0;
+                }
+
+                MapColor mapcolor = (MapColor) Iterables.getFirst(Multisets.copyHighestCountFirst(multiset), MapColor.AIR);
+
+                if (mapcolor == MapColor.WATER)
+                {
+                  d2 = (double)k3 * 0.1D + (double)(k1 + l1 & 1) * 0.2D;
+                  i5 = 1;
+
+                  if (d2 < 0.5D)
+                  {
+                    i5 = 2;
+                  }
+
+                  if (d2 > 0.9D)
+                  {
+                    i5 = 0;
+                  }
+                }
+
+                d0 = d1;
+
+                if (l1 >= 0 && i2 * i2 + j2 * j2 < j1 * j1 && (!flag1 || (k1 + l1 & 1) != 0))
+                {
+                  byte b0 = data.colors[k1 + l1 * 128];
+                  byte b1 = (byte)(mapcolor.colorIndex * 4 + i5);
+
+                  if (b0 != b1)
+                  {
+                    data.colors[k1 + l1 * 128] = b1;
+                    data.updateMapData(k1, l1);
+                    flag = true;
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    return data;
+  }
+
+  private static BufferedImage render(MapData data, Integer scaledRes) {
+    MC.entityRenderer.getMapItemRenderer().renderMap(data, true);
+    ResourceLocation location = findResourceLocation(data.mapName);
+    if (location == null) {
+      Helper.printMessage("Failed to find ResourceLocation");
+      return null;
+    }
+    DynamicTexture texture = (DynamicTexture) MC.getTextureManager().getTexture(location);
+    BufferedImage image = dynamicToImage(texture);
+    if (scaledRes != null) {
+      image = createResizedCopy(image, scaledRes, scaledRes, true);
+    }
+    return image;
+  }
+
+  private static BufferedImage dynamicToImage(DynamicTexture texture) {
+    int[] data = texture.getTextureData();
+    BufferedImage image = new BufferedImage(128, 128, 2);
+    image.setRGB(0, 0, image.getWidth(), image.getHeight(), data, 0, 128);
+    return image;
+  }
+
+  private static ResourceLocation findResourceLocation(String name) {
+    Map<ResourceLocation, ITextureObject> mapTextureObjects =
+        FastReflection.Fields.TextureManager_mapTextureObjects.get(MC.getTextureManager());
+
+    return mapTextureObjects
+        .keySet()
+        .stream()
+        .filter(k -> k.getResourcePath().contains(name))
+        .findFirst()
+        .orElse(null);
+  }
+}
---
+++ src/main/java/com/matt/forgehax/mods/StashFinderMod.java
@@ -0,0 +1,309 @@
+package com.matt.forgehax.mods;
+
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.EvictingQueue;
+import com.matt.forgehax.asm.events.PacketEvent;
+import com.matt.forgehax.events.RenderEvent;
+import com.matt.forgehax.util.command.Setting;
+import com.matt.forgehax.util.mod.Category;
+import com.matt.forgehax.util.mod.ToggleMod;
+import com.matt.forgehax.util.mod.loader.RegisterMod;
+import net.minecraft.block.Block;
+import net.minecraft.client.multiplayer.WorldClient;
+import net.minecraft.init.Blocks;
+import net.minecraft.network.play.server.SPacketChunkData;
+import net.minecraft.tileentity.*;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Level;
+
+import javax.annotation.Nullable;
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.matt.forgehax.Helper.printInform;
+
+//@ClientPlugin
+@RegisterMod
+public class StashFinderMod extends ToggleMod { // implements IClientPlugin {
+
+  private final SimpleDateFormat DATE_FORMATTER = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+
+  private final EvictingQueue<Chunk> chunks = EvictingQueue.create(5120);
+
+//  private IClientAPI jmAPI;
+  private int currentDimension = -1;
+  private File stashFinderDir = null;
+
+  public StashFinderMod() {
+    super(Category.MISC, "StashFinder", false,
+        "Find storage and log them");
+  }
+
+  private final Setting<Boolean> includeChests =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include chests")
+          .description("Clear chunk list on disable")
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Boolean> onlyTrappedChests =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("only-trapped-chests")
+          .description("Clear chunk list on disable")
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Boolean> includeHoppers =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-hoppers")
+          .description("Include hoppers")
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Boolean> includeDispensers =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-dispensers")
+          .description("Include dispensers")
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Boolean> includeFurance =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-furnaces")
+          .description("Include furnaces")
+          .defaultTo(false)
+          .build();
+
+  private final Setting<Boolean> includeEchests =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-echests")
+          .description("Include echests")
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Boolean> includeShulkers =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-shulkers")
+          .description("Include shulkers")
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Boolean> includeBeds =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("include-beds")
+          .description("Include beds")
+          .defaultTo(true)
+          .build();
+
+  private final Setting<Boolean> saveSignData =
+      getCommandStub()
+          .builders()
+          .<Boolean>newSettingBuilder()
+          .name("save-sign-data")
+          .description("Include sign data")
+          .defaultTo(true)
+          .build();
+
+//  private final Setting<Boolean> includeRareItems =
+//      getCommandStub()
+//          .builders()
+//          .<Boolean>newSettingBuilder()
+//          .name("include-rare-items")
+//          .description("Include rare items such as resource blocks, beacons, quartz, etc")
+//          .defaultTo(true)
+//          .build();
+
+  private final Setting<Long> hitThreshold =
+      getCommandStub()
+          .builders()
+          .<Long>newSettingBuilder()
+          .name("entity-threshold")
+          .description(
+              "Number if hits per chunk to trigger a coords log")
+          .defaultTo(10L)
+          .build();
+
+//  private final Setting<Boolean> screenshot =
+//      getCommandStub()
+//          .builders()
+//          .<Boolean>newSettingBuilder()
+//          .name("take-screenshot")
+//          .description("Take screenshot of chunk when hit matches")
+//          .defaultTo(true)
+//          .build();
+
+  @Override
+  protected void onEnabled() {
+    super.onEnabled();
+    stashFinderDir = new File("StashFinder");
+    stashFinderDir.mkdirs();
+  }
+
+  @Override
+  public void disable() {
+    super.disable();
+    chunks.clear();
+  }
+
+  @SubscribeEvent
+  public void onPacketInbound(PacketEvent.Incoming.Pre event) {
+    if (getWorld() == null) {
+      return;
+    }
+    if (event.getPacket() instanceof SPacketChunkData) {
+      SPacketChunkData packet = event.getPacket();
+      Chunk chunk = getWorld().getChunkProvider().loadChunk(packet.getChunkX(), packet.getChunkZ());
+      if (chunk == null) {
+        LOGGER.log(Level.DEBUG, "Could not load chunk " + packet.getChunkX() + " " + packet.getChunkZ());
+        return;
+      } else {
+        chunks.add(chunk);
+      }
+    }
+  }
+
+  @SubscribeEvent
+  public void onRender(RenderEvent event) {
+    while (chunks.size() > 0) {
+      Chunk chunk = chunks.remove();
+      LOGGER.debug("chunk.getTileEntityMap() " + chunk.getTileEntityMap().size());
+      List<TileEntity> inChunk = getWorld().loadedTileEntityList.stream().filter(tileEntity -> MC.world.getChunkFromBlockCoords(tileEntity.getPos()).getPos().equals(chunk.getPos())).collect(Collectors.toList());
+      List<Hit> hits = findHits(inChunk);
+      if (hits.size() >= hitThreshold.get()) {
+        printInform("Found possible stash at " + chunk.getPos().toString());
+        report(chunk, hits);
+      } else {
+        LOGGER.debug("Only received " + hits.size() + " hits");
+      }
+    }
+  }
+
+  private void report(Chunk chunk, List<Hit> hits) {
+    String fileName = DATE_FORMATTER.format(new Date());
+    File file = new File(stashFinderDir, fileName + "-hit.txt");
+    ArrayListMultimap<Block, Hit> hitsMap = ArrayListMultimap.create();
+    hits.forEach(hit -> hitsMap.put(hit.blockType, hit));
+    try (PrintWriter writer = new PrintWriter(new FileOutputStream(file))) {
+      writer.print("Chunk location:");
+      writer.print(" ");
+      writer.println(chunk.getPos());
+      hitsMap.asMap().forEach((key, value) -> writer.println(key.getLocalizedName() + ": " + value.size()));
+      List<Hit> signs = hits.stream().filter(hit -> (hit instanceof SignHit)).collect(Collectors.toList());
+      if (signs.size() > 0) {
+        writer.println("Signs: ");
+        signs.forEach(hit -> {
+          writer.println(((SignHit) hit).signData);
+        });
+      }
+    } catch (IOException e) {
+      LOGGER.error("Couldn't save hit report", e);
+    }
+    saveMap(chunk, fileName);
+    LOGGER.debug("Wrote stash file at " + file.getAbsolutePath());
+  }
+
+  private void saveMap(Chunk chunk, String fileName) {
+    int x = chunk.x << 4;
+    int z = chunk.z << 4;
+    BufferedImage image = MapGenerator.render(getWorld(), x, z);
+    if (image == null) {
+      LOGGER.info("Could not render image");
+    } else {
+      LOGGER.info("Rendered image");
+      File mapFile = new File(stashFinderDir, fileName + "-map.png");
+      try {
+        ImageIO.write(image, "png", mapFile);
+      } catch (IOException e) {
+        LOGGER.error("Could not save image to " + mapFile, e);
+      }
+    }
+  }
+
+  private List<Hit> findHits(Collection<TileEntity> tileEntities) {
+    List<Hit> hits = new ArrayList<>();
+    if (includeChests.get()) {
+      List<Hit> chests = tileEntities.stream().filter(entity -> {
+        if (!(entity instanceof TileEntityChest)) {
+          return false;
+        }
+        return !onlyTrappedChests.get() || entity.getBlockType() == Blocks.TRAPPED_CHEST;
+      }).map(Hit::new).collect(Collectors.toList());
+      hits.addAll(chests);
+    }
+    if (includeShulkers.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityShulkerBox).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (includeEchests.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityEnderChest).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (includeFurance.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityFurnace).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (includeHoppers.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityHopper).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (includeDispensers.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityDispenser).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (includeBeds.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntityBed).map(Hit::new).collect(Collectors.toList()));
+    }
+    if (saveSignData.get()) {
+      hits.addAll(tileEntities.stream().filter(entity -> entity instanceof TileEntitySign).map(tileEntity -> new SignHit((TileEntitySign)tileEntity)).collect(Collectors.toList()));
+    }
+    return hits;
+  }
+
+  private static class Hit {
+    public final Block blockType;
+    public final BlockPos position;
+
+    public Hit(TileEntity tileEntity) {
+      this.blockType = tileEntity.getBlockType();
+      this.position = tileEntity.getPos();
+    }
+  }
+
+  private static class SignHit extends Hit {
+    public final String signData;
+
+    public SignHit(TileEntitySign sign) {
+      super(sign);
+      StringJoiner joiner = new StringJoiner(" / ");
+      Arrays.stream(sign.signText).forEach(iTextComponent -> joiner.add(iTextComponent.getUnformattedText()));
+      this.signData = joiner.toString();
+    }
+  }
+
+  @Nullable
+  public static WorldClient getWorld() {
+    return MC.world;
+  }
+}
